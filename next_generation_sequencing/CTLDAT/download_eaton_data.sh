9fe4d0590503442107d6dd1b7b087f4bf0d25e4f
luised94 <luised94@mit.edu>
2024-04-08 13:01:15 -0400

    dos2unix on dowload_eaton.sh, confirmed functionality

--- next_generation_sequencing/CTLDAT/download_eaton_data.sh
+++ next_generation_sequencing/CTLDAT/download_eaton_data.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-cd /home/luised94/data/221024Bel_CHIP/data/fastq-files/
-pwd
+workdir="."
+#cd /home/luised94/data/221024Bel_CHIP/data/fastq-files/
+#pwd
 
-outputfiles=(WT-G2-ORC-rep1.fastq.gz WT-G2-ORC-rep2.fastq.gz)
+outputfiles=("WT-G2-ORC-rep1.fastq.gz" "WT-G2-ORC-rep2.fastq.gz")
 websites=(ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR034/SRR034475/SRR034475.fastq.gz ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR034/SRR034476/SRR034476.fastq.gz)
 
 COUNTER=0
-for file in ${outputfiles[@]};
-do 
+for file in ${outputfiles[@]}; do 
   echo $file
   echo $COUNTER 
   echo ${websites[COUNTER]}
