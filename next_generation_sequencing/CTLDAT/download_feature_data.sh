b0f9a6cd8f3476d95ff655b0415fa9a623ff3635
luised94 <luised94@mit.edu>
2024-04-08 17:46:23 -0400

    Created download_features_data.sh

---
+++ next_generation_sequencing/CTLDAT/download_feature_data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+download_dir="features"
+
+git clone --depth=1 https://github.com/CEGRcode/2021-Rossi_Nature.git "$download_dir"
+wget -O "$download_dir"/02_References_and_Features_Files/hawkins-origin-timing.xlsx "https://ars.eln-cdn.com/content/image/1-s2.0-S2211124713005834-mmc2.xlsx"
+#"https://www.cell.com/cms/10.1016/j.cellrep.2013.10.014/attachment/a16ab8a7-4e28-4ca6-aafe-e8857b760c80/mmc2.xlsx 
+#"https://ars.eln-cdn.com/content/image/1-s2.0-S2211124713005834-mmc2.xlsx" 
+#mv "$download_dir/CEGRcode/2021-Rossi_Nature/02_References_and_Features_Files" "$download_dir"
+
+
+
+
