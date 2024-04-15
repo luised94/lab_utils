833f9af15457c6adbc403924c3505e1ed428a90e
Luis <luised94@mit.edu>
2024-04-15 14:39:57 -0400

    Renamed REFGEN scripts, filterFastq.sh stable

--- next_generation_sequencing/FTQPRC/test_001_filterFastq.sh
+++ next_generation_sequencing/FTQPRC/test_001_filterFastq.sh
@@ -7,7 +7,7 @@
 #SBATCH --mem-per-cpu=20G # amount of RAM per node#
 #SBATCH --array=1-4%16
 #USAGE: From anywhere, run 'sbatch ~/data/lab_utils/next_generation_sequencing/test_001_filterFastq.sh <dir>'
-
+#SETUP
 DIR_TO_PROCESS="$1"
 
 # Define the log directory
@@ -23,27 +23,36 @@ ERR_FILE="${LOG_DIR}/${timeid}_filtering_${SLURM_ARRAY_JOB_ID}_${SLURM_JOB_ID}_$
 # Redirect stdout and stderr to the respective files
 exec >"$OUT_FILE" 2>"$ERR_FILE"
 
-# Your script's commands follow...
+#LOG
 echo "SLURM_JOB_ID=${SLURM_JOB_ID}, SLURM_ARRAY_JOB_ID=${SLURM_ARRAY_JOB_ID}, SLURM_ARRAY_TASK_ID=${SLURM_ARRAY_TASK_ID}"
 
 echo "Started from $(pwd)"
 echo "START TIME: $(date "+%Y-%m-%d-%M-%S")"
-refgenome_dir="$HOME/data/REFGENS"
-echo "Executing from $DIR_TO_PROCESS"
+DIR_TO_PROCESS="$HOME/data/$DIR_TO_PROCESS"
+echo "Executing for $DIR_TO_PROCESS"
 
+#MODULE_LOAD
 module purge
-module load 
-module load 
+module load fastp/0.20.0
 
-mapfile -t fastq_paths < <(find "${refgenome_dir}" -type f \( -name "*unmapped*" \) -prune -o -name  "*.fastq")
+#INITIALIZE_ARRAY
+mapfile -t fastq_paths < <(find "${DIR_TO_PROCESS}" -type f -name "*.fastq" ! \( -name "*unmapped*" -o -name "processed_*" \))
 
-fastq_path=${genome_paths[$SLURM_ARRAY_TASK_ID-1]}
+#INPUT_OUTPUT
+fastq_path=${fastq_paths[$SLURM_ARRAY_TASK_ID-1]}
+output_path=$(echo "$fastq_path" | cut -d/ -f7 | xargs -I {} echo "${DIR_TO_PROCESS}processed-fastq/processed_{}")
+json_path=$(echo "$fastq_path" | cut -d/ -f7 | xargs -I {} echo "${DIR_TO_PROCESS}logs/processed_{}")
+#LOG
+echo "Starting fitering"
+echo "FASTQ_FILE: $fastq_path"
+echo "OUTPUT_FILE: $output_path"
 
-echo "Starting indexing for $genome_path"
-#Confirmed bowtie2-build works
-#bowtie2-build $fastq_path "${fastq_path%.fastq}_processed.fastq"
-fastp -i in1.fastq  -o out1.fastq --qualified_quality_phred 20 --unqualified_percent_limit 50
+#Confirmed fastp works from node using example file
+#COMMAND_TO_EXECUTE 
+#fastp in slurm file
+echo "-i "${fastq_path}"  -o "${output_path}" --json "${json_path%.fastq}.json" --html /dev/null --cut_window_size 4 --cut_mean_quality 20 --n_base_limit 5 --average_qual 20 --length_required 50 --qualified_quality_phred 20 --unqualified_percent_limit 50"
 
-echo "Indexing completed"
+#COMMAND_OUTPUT_START
+#LOG
+echo "Filtering completed"
 echo "END TIME: $(date "+%Y-%m-%d-%M-%S")"
-
