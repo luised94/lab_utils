2d958987d7a4a96be3024cb2f1803b15bea10ba7
Luis <luised94@mit.edu>
2024-04-17 13:33:23 -0400

    Fixed bowtie2-samtools pipeline, adjusted variable names

--- next_generation_sequencing/FTQPRC/test_002_alignFastq.sh
+++ next_generation_sequencing/FTQPRC/test_002_alignFastq.sh
@@ -3,12 +3,12 @@
 #SBATCH -n 1 # Number of tasks. Don't specify more than 16 unless approved by the system admin
 #SBATCH --mail-type=ALL # Type of email notification- BEGIN,END,FAIL,ALL. Equivalent to the -m option in SGE
 #SBATCH --mail-user=luised94@mit.edu  # Email to which notifications will be sent. Equivalent to the -M option in SGE.
-#SBATCH --exclude=c[5-22]
-#SBATCH --mem-per-cpu=50G # amount of RAM per node#
+#SBATCH --exclude=c[5-22] #Required by MIT
+#SBATCH --mem-per-cpu=50G # amount of RAM per node
 #SBATCH --cpus-per-task=4
-#SBATCH --array=1-10%16
-#SBATCH --nice=10000
-#USAGE: First, determine this by running the INITIALIZE_ARRAY and multiplying by number of genomes, modify the array number. For test, leave at 1-2 to test array creation. Then, from anywhere, run 'sbatch ~/data/lab_utils/next_generation_sequencing/test_002_alignFastq.sh <dir>'
+#SBATCH --array=1-4%16
+#SBATCH --nice=10000 #Required by MIT
+#USAGE: First, determine this by running the INITIALIZE_ARRAY and multiplying by number of genomes, modify the array number. For test, leave at 1-2 to test array creation. Then, from anywhere, run 'sbatch ~/data/lab_utils/next_generation_sequencing/slurm_002_alignFastq.sh <dir>'
 #SETUP
 DIR_TO_PROCESS="$1"
 
@@ -36,37 +36,40 @@ REFGENOME_DIR="$HOME/data/REFGENS"
 
 #MODULE_LOAD
 module purge
+module load gnu/5.4.0
 module load bowtie2/2.3.5.1
 module load samtools/1.10
 
 #INITIALIZE_ARRAY
-mapfile -t fastq_paths < <(find "${DIR_TO_PROCESS}" -type f -name "processed_*.fastq" )
-mapfile -t genomes_paths < <(find "${REFGENOME_DIR}" -type f -name "*_refgenome.fna")
+mapfile -t FASTQ_PATHS < <(find "${DIR_TO_PROCESS}" -type f -name "processed_*.fastq" )
+mapfile -t GENOME_PATHS < <(find "${REFGENOME_DIR}" -type f -name "*_refgenome.fna")
 
 #INPUT_OUTPUT
-#fastq_path=${fastq_paths[$SLURM_ARRAY_TASK_ID-1]}
+#fastq_path=${FASTQ_PATHS[$SLURM_ARRAY_TASK_ID-1]}
 #output_path=$(echo "$fastq_path" | cut -d/ -f7 | xargs -I {} echo "${DIR_TO_PROCESS}processed-fastq/processed_{}")
 
 #LOG
 # Perform indexing by getting quotient and remainder
-GENOME_INDEX=$(( ($SLURM_ARRAY_TASK_ID - 1) / ${#fastq_paths[@]}))
-FASTQ_INDEX=$(( ($SLURM_ARRAY_TASK_ID - 1)  % ${#fastq_paths[@]}))
+GENOME_INDEX=$(( ($SLURM_ARRAY_TASK_ID - 1) / ${#FASTQ_PATHS[@]}))
+FASTQ_INDEX=$(( ($SLURM_ARRAY_TASK_ID - 1)  % ${#FASTQ_PATHS[@]}))
 echo "Processing ${GENOME_INDEX} and ${FASTQ_INDEX}"
 
 #Get names for output
-fastq_ID=$(echo "${fastq_paths[$fastq_index]}" | cut -d_ -f3 )
-genome_name=$( echo "${genomes_paths[$genome_index]}" | cut -d_ -f1 | rev | cut -d/ -f1 | rev )
-echo "$(basename $fastq_ID) | $(basename $genome_name)"
+FASTQ_ID=$(echo "${FASTQ_PATHS[$FASTQ_INDEX]}" | cut -d_ -f3 )
+GENOME_NAME=$( echo "${GENOME_PATHS[$GENOME_INDEX]}" | cut -d_ -f1 | rev | cut -d/ -f1 | rev )
+echo "$(basename $FASTQ_ID) | $(basename $GENOME_NAME)"
 
-echo "Starting fitering"
+echo "Starting alignment"
 #COMMAND_TO_EXECUTE 
 echo "COMMAND_OUTPUT_START"
 #bowtie2 [options]* -x <bt2-idx> {-1 <m1> -2 <m2> | -U <r> | --interleaved <i> | -b <bam>} [-S <sam>]
-echo "bowtie2 -x ${genomes_paths[$GENOME_INDEX]%_refgenome.fna}_index -U ${fastq_paths[$FASTQ_INDEX]} -p $SLURM_CPUS_PER_TASK -q --mp 4 --met-stderr - |"
-echo "samtools view -@ ${SLURM_CPUS_PER_TASK} -bS |"
-echo "samtools sort -@ ${SLURM_CPUS_PER_TASK} -o ${DIR_TO_PROCESS}alignment/${fastq_ID}_${genome_name}.bam "
+#For testing grab a subset of the data using seqtk:seqtk sample [-2] [-s seed=11] <in.fa> <frac>|<number>
+#seqtk sample -s100 your_fastq_file.fastq 0.1 > subsampled_fastq_file.fastq
+echo "bowtie2 -x ${GENOME_PATHS[$GENOME_INDEX]%_refgenome.fna}_index -U ${FASTQ_PATHS[$FASTQ_INDEX]} -p $SLURM_CPUS_PER_TASK -q --mp 4 --met-stderr |
+samtools view -@ ${SLURM_CPUS_PER_TASK} -b - |
+samtools sort -@ ${SLURM_CPUS_PER_TASK} -o ${DIR_TO_PROCESS}alignment/${FASTQ_ID}_${GENOME_NAME}.bam -"
 
-echo "samtools index ${DIR_TO_PROCESS}alignment/${fastq_ID}_${genome_name}.bam"
+echo "samtools index ${DIR_TO_PROCESS}alignment/${FASTQ_ID}_${GENOME_NAME}.bam"
 
 #LOG
 echo "COMMAND_OUTPUT_END"
