e759c472f7e3d079bfc9d5e33a4c95106950782d
Luis <luised94@mit.edu>
2024-04-19 13:07:01 -0400

    Added number prefix to all directories to record order of analysis

--- next_generation_sequencing/000_setupInteractiveSessionVariables.sh
+++ next_generation_sequencing/000_setupInteractiveSessionVariables.sh
@@ -16,7 +16,9 @@ module load samtools/1.10
 module load fastqc
 module load python/2.7.13
 module load deeptools/3.0.1
+module load r/4.2.0
 #INITIALIZE_ARRAY
-mapfile -t FASTQ_PATHS < <(find "${DIR_TO_PROCESS}" -type f -name "processed_*.fastq" )
-mapfile -t GENOME_PATHS < <(find "${REFGENOME_DIR}" -type f -name "*_refgenome.fna")
-mapfile -t BAM_PATHS < <(find "${DIR_TO_PROCESS}" -type f -name "*.bam")
+mapfile -t FASTQ_PATHS < <(find "${DIR_TO_PROCESS}" -type f -name "processed_*.fastq" | sort )
+mapfile -t GENOME_PATHS < <(find "${REFGENOME_DIR}" -type f -name "*_refgenome.fna" )
+mapfile -t BAM_PATHS < <(find "${DIR_TO_PROCESS}" -type f -name "*.bam" | sort)
+mapfile -t BIGWIG_PATHS < <(find "${DIR_TO_PROCESS}" -type f -name "*S288C.bw" | sort )
