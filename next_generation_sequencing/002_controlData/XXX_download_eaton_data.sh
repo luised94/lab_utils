ff43d332a2d8a713092112fe40a15f9d26203e00
Luis <luised94@mit.edu>
2024-04-22 13:43:11 -0400

    XXX_download_eaton_data.sh works! URL had fasta instead of fastq

--- next_generation_sequencing/002_controlData/XXX_download_eaton_data.sh
+++ next_generation_sequencing/002_controlData/XXX_download_eaton_data.sh
@@ -13,15 +13,14 @@ mapfile -t ACCESSION < <(printf "%s\n" SRR0344{75,76})
 
 FILE_INDEX=0
 for FILE in ${FILES_TO_CONCATENATE[@]}; do 
-  OUTPUT_FILE=${OUTPUT_DIR}/$FILE
-  URL_TO_DOWNLOAD=${BASE_URL}${ACCESSION[FILE_INDEX]:0:6}/${ACCESSION[FILE_INDEX]}/${ACCESSION[FILE_INDEX]}.fasta.gz 
-  printf "%s | %d | %s\n" "$FILE_INDEX" "$OUTPUT_FILE" "$URL_TO_DOWNLOAD"
-  curl ${BASE_URL}${ACCESSION[FILE_INDEX]:0:6}/${ACCESSION[FILE_INDEX]}/
+  OUTPUT_FILE=${OUTPUT_DIR}$FILE
+  URL_TO_DOWNLOAD=${BASE_URL}${ACCESSION[FILE_INDEX]:0:6}/${ACCESSION[FILE_INDEX]}/${ACCESSION[FILE_INDEX]}.fastq.gz 
+  printf "%s | %s | %s\n" "$FILE_INDEX" "$OUTPUT_FILE" "$URL_TO_DOWNLOAD"
   curl -I $URL_TO_DOWNLOAD 
 #  wget --output-document=$OUTPUT_FILE $URL_TO_DOWNLOAD
 #  cat $OUTPUT_FILE >> $OUTPUT_DIR/nnNnH.fastq.gz
   ((FILE_INDEX++))
 done 
 
-#gunzip $OUTPUT_DIR/nnNnH.fastq.gz
-echo "gunzip $OUTPUT_DIR/nnNnH.fastq.gz"
+#gunzip ${OUTPUT_DIR}nnNnH.fastq.gz
+echo "gunzip ${OUTPUT_DIR}nnNnH.fastq.gz"
