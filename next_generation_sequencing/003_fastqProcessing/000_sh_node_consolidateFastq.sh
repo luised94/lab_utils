830f72f08b420181b5b4ce63da0567fcd0d26248
Luis <luised94@mit.edu>
2024-04-24 16:55:32 -0400

    Added potential check for read counting inside for loop but havent tested, use grep in commented line to check first few files and they check out, will create clean up now

--- next_generation_sequencing/003_fastqProcessing/000_sh_node_consolidateFastq.sh
+++ next_generation_sequencing/003_fastqProcessing/000_sh_node_consolidateFastq.sh
@@ -9,19 +9,34 @@ mapfile -t FASTQ_PATHS < <(find "${ABSOLUTE_PATH_OF_DIR}" -type f -name "*.fastq
 #Could substitute cut with awk -F '-' '{print $2}'
 UNIQUE_IDS=($(printf '%s\n' "${FASTQ_PATHS[@]}" | cut -d- -f2 | uniq ))
 
-#${1}_D24-${UNIQUE_ID}_NA_sequence.fastq 
+#TODO Incorporate the read checking inside the for loop, not tested currently
+#files_passed_read_check=0
 for UNIQUE_ID in ${UNIQUE_IDS[@]}; do 
 	OUTPUT_FILE="${OUTPUT_DIR}D24-${UNIQUE_ID}_NA_sequence.fastq"
 	echo "Processing ID: ${UNIQUE_ID}, Output: ${OUTPUT_FILE}"
 	#FILTERED_PATHS=($(printf '%s\n' "${FASTQ_PATHS[@]}" | grep "${UNIQUE_ID}")) 
 	#echo "cat "${FILTERED_PATHS[@]}" >> ${OUTPUT_FILE}"
 	echo "Loop starting"
+	#total_reads=0
 	for FASTQ_PATH in "${FASTQ_PATHS[@]}"; do
 		if [[ $FASTQ_PATH =~ $UNIQUE_ID ]]; then 
 			cat "$FASTQ_PATH" >> "$OUTPUT_FILE"
+			#reads_in_file=$(grep -c "^@" "$FASTQ_PATH")
+			#total_reads=$((total_reads + reads_in_file))		
 		fi
 	done
-	
+	#reads_in_output=$(grep -c "^@" "$OUTPUT_FILE")
+	#if [[ $total_reads -eq $reads_in_output ]]; then
+	#	((files_passed_read_check++))
+	#fi	
 done 
+echo "Files passed read check: $(echo ${files_passed_read_check})"
 echo "Files processed: $(echo ${#FASTQ_PATHS[@]})"
 echo "Number of Unique IDS: $(echo ${#UNIQUE_IDS[@]})"
+
+
+#This also works to do a quick check of the concatenated files since I didnt test them but had to move forward.
+#uncatenated_files=$(find ~/data/240304Bel/ -type d -name "*D24*" -exec find {} -type f -name "*.fastq" \;)
+#grep -c "^@" $(find ~/data/240304Bel/fastq -type f )
+#grep -c "^@" ${uncatenated_files[@]}
+#
