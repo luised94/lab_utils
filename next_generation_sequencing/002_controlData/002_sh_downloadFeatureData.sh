3948483c5dd1bbdc5148bf32c2dbfe8c77fe654b
Luis <luised94@mit.edu>
2024-04-24 11:25:41 -0400

    Wasnt redirection but pinging the website also doesnt work. Could be an issue on my end with the vpn, will move on since R works

--- next_generation_sequencing/002_controlData/002_sh_downloadFeatureData.sh
+++ next_generation_sequencing/002_controlData/002_sh_downloadFeatureData.sh
@@ -7,7 +7,6 @@ git clone --depth=1 https://github.com/CEGRcode/2021-Rossi_Nature.git "$download
 #"https://www.cell.com/cms/10.1016/j.cellrep.2013.10.014/attachment/a16ab8a7-4e28-4ca6-aafe-e8857b760c80/mmc2.xlsx 
 #"https://ars.eln-cdn.com/content/image/1-s2.0-S2211124713005834-mmc2.xlsx" 
 #mv "$download_dir/CEGRcode/2021-Rossi_Nature/02_References_and_Features_Files" "$download_dir"
-
-
+#curl --output $HOME/data/playground/test.xlsx "https://ars.eln-cdn.com/content/image/1-s2.0-S2211124713005834-mmc2.xlsx"
 
 
