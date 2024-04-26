155d86ac4f8b1fa595711db09535f1d8fb6989d8
luised94 <luised94@mit.edu>
2024-04-26 13:57:10 -0400

    CHECKPOINT: Trying to reorganize R code. Will use renv to manage packages even if it requires duplicating libraries. Created 001_setupR with installatiin of R4.2.0, modifying the organziation of R code for next_generation_sequencing module

--- 001_setupR/001_R_node_packageInstallation.R
+++ 001_setupR/001_R_node_packageInstallation.R
@@ -1,11 +1,11 @@
 
 # Installs R libraries to library_location.
 # Inside R source the file. 
+# This should be setup in 000_installingR4.2.0.sh
+#home_directory <- Sys.getenv("HOME")
+#library_location <- paste(home_directory, "R/x86_64-pc-linux-gnu-library/4.2", sep = "/")
 
-home_directory <- Sys.getenv("HOME")
-library_location <- paste(home_directory, "R/x86_64-pc-linux-gnu-library/4.2", sep = "/")
-
-install.packeges(c("tidyverse", "R.utils", "ggplot2", "BiocManager"),
+install.packeges(c("tidyverse", "R.utils", "ggplot2", "BiocManager", "renv"),
 		lib = library_location)
 # If there is an error because of the location of the file, you can use .libPaths() to adjust the location that will be used to install. See one line below.
 #.libPaths( library_location, .libPaths())
---
+++ next_generation_sequencing/000_R_node_packageInstallation.R
@@ -0,0 +1,11 @@
+# 
+#
+#home_directory <- Sys.getenv("HOME")
+#library_location <- paste(home_directory, "R/x86_64-pc-linux-gnu-library/4.2", sep = "/")
+#Check that library to installs is correct.
+#.libPaths()
+
+library(BiocManager)
+bioconductor_packages_to_install <- c("QuasR", "GenomicAlignments", "Gviz", "rtracklayer", "ShortRead")
+BiocManager::install(bioconductor_packages_to_install
+		lib = library_location)
