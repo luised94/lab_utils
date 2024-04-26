756fd8025791beb65e51d9ce4b46b1103e4b8aa0
Luis <luised94@mit.edu>
2024-04-26 17:28:45 -0400

    Added print statement and some formatting to 000_sh_node_slurmWrapper.sh for better readibility

--- linux_cluster/000_sh_node_slurmWrapper.sh
+++ linux_cluster/000_sh_node_slurmWrapper.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 #USAGE: 
 #~/lab_utils/next_generation_sequencing/linux_cluster/000_sh_node_slurmWrapper.sh 1-N%16 000_scriptToRun.sh 240304Bel
+echo -e "Executing from $(pwd) \n"
+
 if [ $# -ne 3 ]; then
-	echo "Usage: $0 <array_number> <<script_name> <DIRECTORY_TO_PROCESS_to_process>" 
-	echo 'Array number is an integer or range (1-N%16) depending on the number of array tasks to create (--array= option for SBATCH)' 
-	echo 'script_name is basename of lab_utils script ( eg 000_sh_node_test_slurmWrapper.sh )' 
-	echo 'Directory is DIRECTORY_TO_PROCESS to process with / ( eg 240304Bel/ )' 
+	echo -e "Usage: \n $0 <array_number> <script_name> <DIRECTORY_TO_PROCESS_to_process> \n" 
+	echo -e 'Array number is an integer or range (1-N%16) depending on the number of array tasks to create. 1 is an option. (--array= option for SBATCH) \n' 
+	echo -e 'script_name is basename of lab_utils script ( eg 000_sh_node_test_slurmWrapper.sh ) \n' 
+	echo -e 'Directory is name of directory without /. Must be in ~/data ( eg 240304Bel ) \n' 
 	exit 1
 fi
 
@@ -13,8 +15,8 @@ array_range="$1"
 SCRIPT_TO_RUN=$(find $HOME/lab_utils -type f -name "$2")
 DIRECTORY_TO_PROCESS="$(find -H $HOME/data -maxdepth 1 -type d -name "$3")"
 
-echo "Will process $DIRECTORY_TO_PROCESS"
-echo "Will run $SCRIPT_TO_RUN"
+echo "Will process $DIRECTORY_TO_PROCESS \n"
+echo "Will run $SCRIPT_TO_RUN \n"
 
 # Check if script and DIRECTORY_TO_PROCESS exist
 if [ ! -f "$SCRIPT_TO_RUN" -o ! -d "$DIRECTORY_TO_PROCESS" ]; then
