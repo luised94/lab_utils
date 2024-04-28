591ad55a6de043e1b94095de83d124f9a0071f96
luised94 <luised94@mit.edu>
2024-04-28 17:10:41 -0400

    Figuring out the necessary code to download flowCore using renv. Currently having trouble with BH library. Will install from source "Merge into main"

--- 001_setupR/001_packageInstallation.R
+++ 001_setupR/001_packageInstallation.R
@@ -14,3 +14,24 @@ library(BiocManager)
 bioconductor_packages_to_install <- c("QuasR", "GenomicAlignments", "Gviz", "rtracklayer", "ShortRead")
 BiocManager::install(bioconductor_packages_to_install
         lib = library_location)
+#Renv has to be installed overall before it can be used to install 
+#install.packeges("renv")
+renv::init(bioconductor = "3.16")
+# May need to install run in command line because of systemfonts, textshaping, and ragg:
+# sudo apt-get install libharfbuzz-dev libfribidi-dev libfontconfig1-dev libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev libboost-all-dev build-essential
+renv::install(c("tidyverse", "R.utils", "ggplot2", "BiocManager"))
+
+options(repos = BiocManager::repositories())
+
+#
+#renv::install("BH@1.75.0-0")
+renv::install(c("flowCore"))
+
+library(flowCore)
+
+renv::snapshot()
+
+#library(BiocManager)
+#bioconductor_packages_to_install <- c("QuasR", "GenomicAlignments", "Gviz", "rtracklayer", "ShortRead")
+#BiocManager::install(bioconductor_packages_to_install
+#		lib = library_location)
