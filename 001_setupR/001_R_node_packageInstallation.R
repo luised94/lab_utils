4b5c7f820666e82d8bd5b079382f0a7d78d0bd20
luised94 <luised94@mit.edu>
2024-04-28 17:10:41 -0400

    Figuring out the necessary code to download flowCore using renv. Currently having trouble with BH library. Will install from source

--- 001_setupR/001_R_node_packageInstallation.R
+++ 001_setupR/001_R_node_packageInstallation.R
@@ -5,12 +5,24 @@
 #home_directory <- Sys.getenv("HOME")
 #library_location <- paste(home_directory, "R/x86_64-pc-linux-gnu-library/4.2", sep = "/")
 
-install.packeges(c("tidyverse", "R.utils", "ggplot2", "BiocManager", "renv"),
-		lib = library_location)
-# If there is an error because of the location of the file, you can use .libPaths() to adjust the location that will be used to install. See one line below.
-#.libPaths( library_location, .libPaths())
+#Renv has to be installed overall before it can be used to install 
+#install.packeges("renv")
+renv::init(bioconductor = "3.16")
+# May need to install run in command line because of systemfonts, textshaping, and ragg:
+# sudo apt-get install libharfbuzz-dev libfribidi-dev libfontconfig1-dev libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev libboost-all-dev build-essential
+renv::install(c("tidyverse", "R.utils", "ggplot2", "BiocManager"))
 
-library(BiocManager)
-bioconductor_packages_to_install <- c("QuasR", "GenomicAlignments", "Gviz", "rtracklayer", "ShortRead")
-BiocManager::install(bioconductor_packages_to_install
-		lib = library_location)
+options(repos = BiocManager::repositories())
+
+#
+#renv::install("BH@1.75.0-0")
+renv::install(c("flowCore"))
+
+library(flowCore)
+
+renv::snapshot()
+
+#library(BiocManager)
+#bioconductor_packages_to_install <- c("QuasR", "GenomicAlignments", "Gviz", "rtracklayer", "ShortRead")
+#BiocManager::install(bioconductor_packages_to_install
+#		lib = library_location)
