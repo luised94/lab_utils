6ff893f2bd87a5682adfa8860cc6117bc7d2132b
luised94 <luised94@mit.edu>
2024-04-29 18:07:14 -0400

    Added svglite to save plots to svg, created different examples of plots using ggplot2, need to figure out, data grouping and to plot using grid or as staggered

--- flow_cytometry/001_R_node_packageInstallation.R
+++ flow_cytometry/001_R_node_packageInstallation.R
@@ -1,22 +1,7 @@
-# Installs R libraries to library_location.
-# Inside R source the file. 
-# This should be setup in 001_setupR/000_installingR4.2.0.sh
-#home_directory <- Sys.getenv("HOME")
-#library_location <- paste(home_directory, "R/x86_64-pc-linux-gnu-library/4.2", sep = "/")
-
-install.packeges(c("tidyverse", "R.utils", "ggplot2", "BiocManager", "renv"),
-        lib = library_location)
-# If there is an error because of the location of the file, you can use .libPaths() to adjust the location that will be used to install. See one line below.
-#.libPaths( library_location, .libPaths())
-
-library(BiocManager)
-bioconductor_packages_to_install <- c("QuasR", "GenomicAlignments", "Gviz", "rtracklayer", "ShortRead")
-BiocManager::install(bioconductor_packages_to_install
-        lib = library_location)
-#Renv has to be installed overall before it can be used to install 
-#install.packeges("renv")
-renv::init(bioconductor = "3.16")
-renv::install(c("tidyverse", "R.utils", "ggplot2", "BiocManager", "remotes","devtools", "svglite","gtools"))
+# May need to install run in command line because of systemfonts, textshaping, and ragg:
+# sudo apt-get install libharfbuzz-dev libfribidi-dev libfontconfig1-dev libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev libboost-all-dev build-essential
+renv::install(c("tidyverse", "R.utils", "ggplot2", "BiocManager", "remotes","devtools", "svglite"))
+>>>>>>> 56ecfcc (Added svglite to save plots to svg, created different examples of plots using ggplot2, need to figure out, data grouping and to plot using grid or as staggered)
 
 repository <- "github::"
 user <- "RGLab/"
