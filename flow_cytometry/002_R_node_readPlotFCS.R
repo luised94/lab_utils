56ecfcc9dcf52088856f916f025275c8d838f1d1
luised94 <luised94@mit.edu>
2024-04-29 18:07:14 -0400

    Added svglite to save plots to svg, created different examples of plots using ggplot2, need to figure out, data grouping and to plot using grid or as staggered

--- flow_cytometry/002_R_node_readPlotFCS.R
+++ flow_cytometry/002_R_node_readPlotFCS.R
@@ -1,6 +1,6 @@
 library(flowCore)
 library(ggcyto)
-
+library(svglite)
 # Single file
 fcs_data <- read.FCS("path_to_your_file.fcs")
 
@@ -24,4 +24,38 @@ print(p)
 p <- ggcyto(gs, aes(x = `FL1-H`)) + geom_histogram(data = "CD3+")  # Subset to CD3+ population
 print(p)
 
+path_to_fcs_files <- "/mnt/c/Users/Luis/Dropbox (MIT)/Lab/Experiments/Yeast Genetics/2023_10_04 Cdc6 Overexpression/CL4_Cdc6Oe_CarbonSourceShift_01"
+fcs_file_paths <- list.files(path_to_fcs_files, pattern = "\\.fcs$", full.names = TRUE)
+fcs_data <- read.FCS(fcs_file_paths[1])
+colnames(head(exprs(fcs_data)))
+head(exprs(fcs_data)[,11])
+head(exprs(fcs_data[,'FITC-Width']))
 
+output_directory <- "/mnt/c/Users/Luis/Dropbox (MIT)/Lab/Experiments/Yeast Genetics/2023_10_04 Cdc6 Overexpression/"
+
+plot_output <- paste0(output_directory, "/", "plot_hist.svg")
+svglite(plot_output, width = 6, height = 4)
+hist(exprs(fcs_data[,'FITC-Width']),
+	  main = "Histogram of FITC-Width",
+	  xlab = "FITC-Width",
+	  col = "blue",
+	  border = "black")
+dev.off()
+
+plot_output <- paste0(output_directory, "/", "plot_density.svg")
+svglite(plot_output, width = 6, height = 4)
+ggplot(fcs_data, aes(x = `FITC-Width`)) +
+	geom_histogram(aes(y = after_stat(density)), binwidth = 30, colour = "black", fill = "white") +
+	geom_density(alpha = .2, fill = "#FF6666") +
+	labs(title = "Fluorescence Intensity Distribution", x = "Fluorescence Intensity", y = "Density")
+dev.off()
+
+plot_output <- paste0(output_directory, "/", "plot_count.svg")
+svglite(plot_output, width = 6, height = 4)
+ggplot(fcs_data, aes(x = `FITC-Width`)) +
+	geom_histogram(aes(y = after_stat(count)), binwidth = 30, colour = "black", fill = "white") +
+	geom_density(alpha = .2, fill = "#FF6666") +
+	labs(title = "Fluorescence Intensity Distribution", x = "Fluorescence Intensity", y = "Density")
+dev.off()
+
+summary(fcs_data)
