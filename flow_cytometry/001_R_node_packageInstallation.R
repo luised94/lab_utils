4649426997c33ffb969668881b2a1b9ca7fdbf8b
luised94 <luised94@mit.edu>
2024-04-29 13:03:50 -0400

    Changed devtools to using renv::install for github packages to be more consistent. Used variables and paste for installation. confirmed installation with library()

--- flow_cytometry/001_R_node_packageInstallation.R
+++ flow_cytometry/001_R_node_packageInstallation.R
@@ -4,7 +4,6 @@
 #home_directory <- Sys.getenv("HOME")
 #library_location <- paste(home_directory, "R/x86_64-pc-linux-gnu-library/4.2", sep = "/")
 
-<<<<<<< HEAD
 install.packeges(c("tidyverse", "R.utils", "ggplot2", "BiocManager", "renv"),
         lib = library_location)
 # If there is an error because of the location of the file, you can use .libPaths() to adjust the location that will be used to install. See one line below.
@@ -14,40 +13,21 @@ library(BiocManager)
 bioconductor_packages_to_install <- c("QuasR", "GenomicAlignments", "Gviz", "rtracklayer", "ShortRead")
 BiocManager::install(bioconductor_packages_to_install
         lib = library_location)
-=======
->>>>>>> 51cccf6 (Managed to load flowCore, was having problems installing using BiocManager because of a BOOST library problem, installing from github worked. Creadted readPlotFCS.R as a starting point, considering copying some of the R code from next_generation and atpase code that I havent integrated)
 #Renv has to be installed overall before it can be used to install 
 #install.packeges("renv")
 renv::init(bioconductor = "3.16")
-# May need to install run in command line because of systemfonts, textshaping, and ragg:
-# sudo apt-get install libharfbuzz-dev libfribidi-dev libfontconfig1-dev libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev libboost-all-dev build-essential
-<<<<<<< HEAD
 renv::install(c("tidyverse", "R.utils", "ggplot2", "BiocManager", "remotes","devtools", "svglite","gtools"))
 
-options(repos = BiocManager::repositories())
-
-#Wasted a bunch of time trying to figure out how to install flowCore dependencies from source. Solved it by installing through github.
 repository <- "github::"
 user <- "RGLab/"
 packages <- c("RProtoBufLib", "cytolib", "flowCore")
 packages_to_install <- paste0(repository, user, packages)
 renv::install(c(packages_to_install, "ggcyto"))
-=======
 renv::install(c("tidyverse", "R.utils", "ggplot2", "BiocManager", "remotes","devtools"))
 
 options(repos = BiocManager::repositories())
-#Wasted a bunch of time trying to figure out how to install flowCore dependencies from source. Solved it by installing through github.
-devtools::install_github("RGLab/RProtoBufLib")
-devtools::install_github("RGLab/cytolib")
-devtools::install_github("RGLab/flowCore")
-renv::install("ggcyto")
->>>>>>> 51cccf6 (Managed to load flowCore, was having problems installing using BiocManager because of a BOOST library problem, installing from github worked. Creadted readPlotFCS.R as a starting point, considering copying some of the R code from next_generation and atpase code that I havent integrated)
 
 library(flowCore)
 
 renv::snapshot()
 
-#library(BiocManager)
-#bioconductor_packages_to_install <- c("QuasR", "GenomicAlignments", "Gviz", "rtracklayer", "ShortRead")
-#BiocManager::install(bioconductor_packages_to_install
-#		lib = library_location)
