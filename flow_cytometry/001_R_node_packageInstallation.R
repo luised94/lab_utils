c9be48b6a7637a4916b77415aa58bb62f623d357
luised94 <luised94@mit.edu>
2024-04-29 18:07:14 -0400

    Added svglite to save plots to svg, created different examples of plots using ggplot2, need to figure out, data grouping and to plot using grid or as staggered

--- flow_cytometry/001_R_node_packageInstallation.R
+++ flow_cytometry/001_R_node_packageInstallation.R
@@ -18,7 +18,7 @@ BiocManager::install(bioconductor_packages_to_install
 renv::init(bioconductor = "3.16")
 # May need to install run in command line because of systemfonts, textshaping, and ragg:
 # sudo apt-get install libharfbuzz-dev libfribidi-dev libfontconfig1-dev libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev libboost-all-dev build-essential
-renv::install(c("tidyverse", "R.utils", "ggplot2", "BiocManager", "remotes","devtools"))
+renv::install(c("tidyverse", "R.utils", "ggplot2", "BiocManager", "remotes","devtools", "svglite"))
 
 options(repos = BiocManager::repositories())
 
