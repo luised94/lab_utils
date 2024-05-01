3dbc8aacddcb1d9cbf66c96cf35e39b5ec17bb0e
luised94 <luised94@mit.edu>
2024-05-01 19:23:33 -0400

    Initialized dataframe using expand.grid which file path is added to after mixedsorting, need to finish task plot fcs data, have thread

--- flow_cytometry/002_R_node_readPlotFCS.R
+++ flow_cytometry/002_R_node_readPlotFCS.R
@@ -1,28 +1,41 @@
 library(flowCore)
-library(ggcyto)
+library(tidyverse)
 library(svglite)
+library(ggplot2)
 
-#path_to_fcs_files <- "/mnt/c/Users/Luis/Dropbox (MIT)/Lab/Experiments/Yeast Genetics/2023_10_04 Cdc6 Overexpression/CL4_Cdc6Oe_CarbonSourceShift_01"
+path_to_fcs_files <- "/mnt/c/Users/Luis/Dropbox (MIT)/Lab/Experiments/Yeast Genetics/2023_10_04 Cdc6 Overexpression/CL4_Cdc6Oe_CarbonSourceShift_01"
 fcs_file_paths <- list.files(path_to_fcs_files, pattern = "\\.fcs$", full.names = TRUE)
-fcs_data <- read.FCS(fcs_file_paths[1])
+#fcs_data <- read.FCS(fcs_file_paths[1])
 
 #TODO: Initialize variables. for each one, use length to assign module class. then maye use switch or some other tidyverse to replace values with variables.
-#REMINDER: Calculated the types of samples that would account for my FACS samples. Seems like I had 4 genotypes, 2 carbon sources and 6 timepoints.
+#REMINDER: Calculated the types of samples that would account for my FACS samples. Seems like I had 4 genotypes, 2 carbon sources and 7 timepoints.
 genotypes <- c("ORC4", "orc4-R267A", "ORC1", "orc1-K485T")
 carbon_sources <- c("GLU", "GAL")
-time_points <- c(-120, seq(0, 150, by = 30))
-for (i in 1:length(fcs_file_paths)){
-#	print(i)
-	formatted_output <- sprintf("%s | %s | %s", i, i %% 7, i %/% 7)
+time_points <- c(seq(0, 150, by = 30))
+df_sample_info <- as.data.frame(expand.grid(genotype_Orc4 = genotypes, 
+					    carbonSources = carbon_sources, 
+					    timePoints = time_points)) %>% arrange(genotype_Orc4,carbonSources,timePoints)
+df_sample_info$filePath <- gtools::mixedsort(fcs_file_paths)#
+is_WTandGLU <- df_sample_info$genotype_Orc4 == "ORC4" & df_sample_info$carbonSources == "GLU"
+subset_df <- df_sample_info %>% filter(is_WTandGLU)
+for (i in 1:nrow(subset_df)) {
+#	flow_data <- read.FCS(df_sample_info$filePath[i])
+#	exprs(flow_data[,'FITC-Width')
+	formatted_output <- sprintf("%s | %s ", i, basename(subset_df$filePath[i]))
 	print(formatted_output)
 }
-
-
-
 colnames(head(exprs(fcs_data)))
 head(exprs(fcs_data)[,11])
 head(exprs(fcs_data[,'FITC-Width']))
-
+#df_test <- apply(df_sample_info, 1, factor)
+#for (i in 1:ncol(df_sample_info)){
+#	levels(df_sample_info[,i]) <- as.factor(df_sample_info[,i])
+#}
+#for (i in 1:length(fcs_file_paths)){
+##	print(i)
+#	formatted_output <- sprintf("%s | %s | %s", i, i %% 7, i %/% 7)
+#	print(formatted_output)
+#}
 output_directory <- "/mnt/c/Users/Luis/Dropbox (MIT)/Lab/Experiments/Yeast Genetics/2023_10_04 Cdc6 Overexpression/"
 
 plot_output <- paste0(output_directory, "/", "plot_hist.svg")
