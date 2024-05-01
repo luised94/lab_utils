7325d41f43bedde567bf3b65878c405a92468354
luised94 <luised94@mit.edu>
2024-04-30 20:57:39 -0400

    Added test for loop to see indexing, Wrote #TODO for how to categorize each sample.

--- flow_cytometry/002_R_node_readPlotFCS.R
+++ flow_cytometry/002_R_node_readPlotFCS.R
@@ -1,32 +1,23 @@
 library(flowCore)
 library(ggcyto)
 library(svglite)
-# Single file
-fcs_data <- read.FCS("path_to_your_file.fcs")
 
-# Multiple files
-fcs_files <- list.files(path = "path_to_fcs_files", pattern = "\\.fcs$", full.names = TRUE)
-fcs_set <- read.flowSet(files = fcs_files)
-
-# Plotting a single parameter histogram
-p <- ggcyto(fcs_data, aes(x = `FL1-H`)) + geom_histogram(bins = 30)
-print(p)
-
-# Plotting a scatter plot of two parameters
-p <- ggcyto(fcs_data, aes(x = `FSC-A`, y = `SSC-A`)) + geom_point()
-print(p)
-
-# Using facet_wrap with a flowSet
-p <- ggcyto(fcs_set, aes(x = `FL1-H`)) + geom_histogram(bins = 30) + facet_wrap(~ name)
-print(p)
-
-# Assuming `gs` is a GatingSet with gates applied
-p <- ggcyto(gs, aes(x = `FL1-H`)) + geom_histogram(data = "CD3+")  # Subset to CD3+ population
-print(p)
-
-path_to_fcs_files <- "/mnt/c/Users/Luis/Dropbox (MIT)/Lab/Experiments/Yeast Genetics/2023_10_04 Cdc6 Overexpression/CL4_Cdc6Oe_CarbonSourceShift_01"
+#path_to_fcs_files <- "/mnt/c/Users/Luis/Dropbox (MIT)/Lab/Experiments/Yeast Genetics/2023_10_04 Cdc6 Overexpression/CL4_Cdc6Oe_CarbonSourceShift_01"
 fcs_file_paths <- list.files(path_to_fcs_files, pattern = "\\.fcs$", full.names = TRUE)
 fcs_data <- read.FCS(fcs_file_paths[1])
+
+#TODO: Initialize variables. for each one, use length to assign module class. then maye use switch or some other tidyverse to replace values with variables.
+genotypes <- 
+cell_cycle <- 
+time_points <-
+for (i in 1:length(fcs_file_paths)){
+#	print(i)
+	formatted_output <- sprintf("%s | %s | %s", i, i %% 7, i %/% 7)
+	print(formatted_output)
+}
+
+
+
 colnames(head(exprs(fcs_data)))
 head(exprs(fcs_data)[,11])
 head(exprs(fcs_data[,'FITC-Width']))
@@ -59,3 +50,17 @@ ggplot(fcs_data, aes(x = `FITC-Width`)) +
 dev.off()
 
 summary(fcs_data)
+#JUNK
+
+## Plotting a single parameter histogram
+#p <- ggcyto(fcs_data, aes(x = `FL1-H`)) + geom_histogram(bins = 30)
+#print(p)
+## Plotting a scatter plot of two parameters
+#p <- ggcyto(fcs_data, aes(x = `FSC-A`, y = `SSC-A`)) + geom_point()
+#print(p)
+## Using facet_wrap with a flowSet
+#p <- ggcyto(fcs_set, aes(x = `FL1-H`)) + geom_histogram(bins = 30) + facet_wrap(~ name)
+#print(p)
+## Assuming `gs` is a GatingSet with gates applied
+#p <- ggcyto(gs, aes(x = `FL1-H`)) + geom_histogram(data = "CD3+")  # Subset to CD3+ population
+#print(p)
