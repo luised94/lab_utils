00b7ee3a9cd8d6402ee1e8a74f67804781efd6cc
luised94 <luised94@mit.edu>
2024-05-02 21:45:07 -0400

    Added Roxygen2-type documentation to functions, created get_datetime and extract_fitc with more descriptive name and error handling, created the different sections to plot in three different ways, first one works but needs adjustment to color gradient. Second way isnt working, will try ggridges for now and maybe see if I can do it with just ggplot2. Finally need to troubleshoot the third way but it looked good and adjusted xlim(), need to use better color gradient

--- flow_cytometry/002_R_node_readPlotFCS.R
+++ flow_cytometry/002_R_node_readPlotFCS.R
@@ -2,17 +2,54 @@ library(flowCore)
 library(tidyverse)
 library(svglite)
 library(ggplot2)
+library(ggridges)
 
 
-read_and_extract <- function(file_path) {
-	fcs_data <- read.FCS(file_path, transformation = FALSE)
-	data_frame <- data.frame(FITC_Width = exprs(fcs_data[,'FITC-Width']))
-	return(data_frame)
+#' Extract FITC Width Data from an FCS File
+#'
+#' This function reads an FCS file and extracts the fluorescence data
+#' corresponding to the FITC width of individual cells.
+#'
+#' @param file_path The path to the FCS file.
+#' @return A data frame with one column, FITC_Width, containing the extracted data.
+#' @examples
+#' extract_fitc_width_from_fcs("path/to/your/file.fcs")
+#' @export
+extract_fitc_width_from_fcs <- function(file_path) {
+	    if (!file.exists(file_path)) {
+		            stop("File does not exist: ", file_path)
+    }
+    
+    fcs_data <- tryCatch({
+	            read.FCS(file_path, transformation = FALSE)
+		        }, error = function(e) {
+				        stop("Failed to read FCS file: ", e$message)
+		        })
+        
+        if (!'FITC-Width' %in% colnames(exprs(fcs_data))) {
+		        stop("FITC-Width data not found in the file.")
+	    }
+        
+        data_frame <- data.frame(FITC_Width = exprs(fcs_data[,'FITC-Width']))
+	    return(data_frame)
 }
 
+#' Generate a Current Datetime String for File Naming
+#'
+#' This function returns a string representing the current date and time,
+#' formatted for use in file names. The format used is "YYYY-MM-DD-HH-MM-SS",
+#' and the system's local time zone is assumed.
+#'
+#' @return A character string of the current date and time.
+#' @examples
+#' get_current_datetime_string()
+#' @export
+get_current_datetime_string <- function() {
+	          return(format(Sys.time(), "%Y-%m-%d-%H-%M-%S"))
+}
+#####
 
-
-windows_user <- list.files("/mnt/c/Users")[grepl(Sys.info()[["user"]], list.files("/mnt/c/Users"))]
+windows_user <- list.files("/mnt/c/Users")[grepl(Sys.info()[["user"]], list.files("/mnt/c/Users"), ignore.case = TRUE)]
 path_to_fcs_files <- sprintf("/mnt/c/Users/%s/Dropbox (MIT)/Lab/Experiments/Yeast Genetics/2023_10_04 Cdc6 Overexpression/CL4_Cdc6Oe_CarbonSourceShift_01", windows_user)
 fcs_file_paths <- list.files(path_to_fcs_files, pattern = "\\.fcs$", full.names = TRUE)
 #fcs_data <- read.FCS(fcs_file_paths[1])
@@ -29,63 +66,67 @@ df_sample_info$filePath <- gtools::mixedsort(fcs_file_paths)#
 is_WTandGLU <- df_sample_info$genotype_Orc4 == "ORC4" & df_sample_info$carbonSources == "GLU"
 subset_df <- df_sample_info %>% filter(is_WTandGLU)
 
+#Check the first six to ensure creation went well. 
+for (i in 1:6) {
+#	flow_data <- read.FCS(df_sample_info$filePath[i])
+#	exprs(flow_data[,'FITC-Width')
+	formatted_output <- sprintf("%s | %s ", i, basename(subset_df$filePath[i]))
+	print(formatted_output)
+}
 
-df$fitc_data <- map(df$file_path, read_and_extract)
-combined_data <- bind_rows(df$fitc_data, .id = "sample_id")
+subset_df$fitcData <- map(subset_df$filePath, extract_fitc_width_from_fcs)
+combined_data <- bind_rows(subset_df$fitcData, .id = "sample_id")
 
-test <- map(df_sample_info$filePath, read_and_extract)
-combined_data <- bind_rows(test, .id = "sample_id")
-
-#for (i in 1:nrow(subset_df)) {
-##	flow_data <- read.FCS(df_sample_info$filePath[i])
-##	exprs(flow_data[,'FITC-Width')
-#	formatted_output <- sprintf("%s | %s ", i, basename(subset_df$filePath[i]))
-#	print(formatted_output)
-#}
-
-
-colnames(head(exprs(fcs_data)))
-head(exprs(fcs_data)[,11])
-head(exprs(fcs_data[,'FITC-Width']))
-#df_test <- apply(df_sample_info, 1, factor)
-#for (i in 1:ncol(df_sample_info)){
-#	levels(df_sample_info[,i]) <- as.factor(df_sample_info[,i])
-#}
-#for (i in 1:length(fcs_file_paths)){
-##	print(i)
-#	formatted_output <- sprintf("%s | %s | %s", i, i %% 7, i %/% 7)
-#	print(formatted_output)
-#}
-output_directory <- "/mnt/c/Users/Luis/Dropbox (MIT)/Lab/Experiments/Yeast Genetics/2023_10_04 Cdc6 Overexpression/"
-
-plot_output <- paste0(output_directory, "/", "plot_hist.svg")
+head(combined_data)
+dim(combined_data)
+str(combined_data)
+summary(combined_data)
+#Plot the data on the same plot and use color gradient to see. 
+output_directory <- sprintf("/mnt/c/Users/%s/Dropbox (MIT)/Lab/Experiments/Yeast Genetics/2023_10_04 Cdc6 Overexpression/", windows_user)
+plot_output <- paste0(output_directory, get_current_datetime_string(), "_densityPlot.svg")
 svglite(plot_output, width = 6, height = 4)
-hist(exprs(fcs_data[,'FITC-Width']),
-	  main = "Histogram of FITC-Width",
-	  xlab = "FITC-Width",
-	  col = "blue",
-	  border = "black")
-dev.off()
-
-plot_output <- paste0(output_directory, "/", "plot_density.svg")
-svglite(plot_output, width = 6, height = 4)
-ggplot(fcs_data, aes(x = `FITC-Width`)) +
-	geom_histogram(aes(y = after_stat(density)), binwidth = 30, colour = "black", fill = "white") +
-	geom_density(alpha = .2, fill = "#FF6666") +
+ggplot(combined_data, aes(x = `FITC.Width`, y = after_stat(density), fill = sample_id)) +
+	geom_density(alpha = .2) +
+	xlim(0, median(combined_data$FITC.Width) + 1500) + 
 	labs(title = "Fluorescence Intensity Distribution", x = "Fluorescence Intensity", y = "Density")
 dev.off()
 
-plot_output <- paste0(output_directory, "/", "plot_count.svg")
+
+# Plot a staggered version using an offset of the samples on the same plot. 
+combined_data$offset <- as.numeric(as.factor(combined_data$sample_id)) * 100 # Create the offset by multiplying sample id with 10
+plot_output <- paste0(output_directory, get_current_datetime_string(), "_plot_count.svg")
 svglite(plot_output, width = 6, height = 4)
-ggplot(fcs_data, aes(x = `FITC-Width`)) +
-	geom_histogram(aes(y = after_stat(count)), binwidth = 30, colour = "black", fill = "white") +
-	geom_density(alpha = .2, fill = "#FF6666") +
+ggplot(combined_data, aes(x = `FITC.Width`, y = after_stat(density), fill = sample_id)) +
+	geom_density(alpha = .2, position = position_nudge(y = combined_data$offset)) +
+	xlim(0, median(combined_data$FITC.Width) + 1500) +
 	labs(title = "Fluorescence Intensity Distribution", x = "Fluorescence Intensity", y = "Density")
 dev.off()
 
-summary(fcs_data)
+#Use a facet grid to plot the samples on different plots
+plot_output <- paste0(output_directory, get_current_datetime_string(), "_plot_count.svg")
+svglite(plot_output, width = 6, height = 4)
+ggplot(combined_data, aes(x = `FITC.Width`)) +
+        geom_density(alpha = .2, fill = "#FF6666") +
+	facet_grid(rows = vars(sample_id)) +
+	xlim(0, median(combined_data$FITC.Width) + 1500) + 
+        labs(title = "Fluorescence Intensity Distribution", x = "Fluorescence Intensity", y = "Density")
+dev.off()
+
+
+
+
+
+
 #JUNK
-
+#scale_fill_gradient(colors = rainbow(length(unique(combined_data$sample_id)))) + 
+#plot_output <- paste0(output_directory, "/", "plot_hist.svg")
+#svglite(plot_output, width = 6, height = 4)
+#hist(exprs(fcs_data[,'FITC-Width']),
+#	  main = "Histogram of FITC-Width",
+#	  xlab = "FITC-Width",
+#	  col = "blue",
+#	  border = "black")
+#dev.off()
 ## Plotting a single parameter histogram
 #p <- ggcyto(fcs_data, aes(x = `FL1-H`)) + geom_histogram(bins = 30)
 #print(p)
