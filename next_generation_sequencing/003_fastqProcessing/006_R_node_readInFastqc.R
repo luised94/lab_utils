f8a8999218d799ceff845452840a52677f6a7263
Luis <luised94@mit.edu>
2024-05-09 15:06:25 -0400

    Add line to disable warning whenever I start R or run Rscript, modified bashrc with export values. Troubleshooting 006_R_node_readInFastqc.R, will use readLines and process all files at same time.

--- next_generation_sequencing/003_fastqProcessing/006_R_node_readInFastqc.R
+++ next_generation_sequencing/003_fastqProcessing/006_R_node_readInFastqc.R
@@ -1,68 +1,46 @@
+Sys.setlocale("LC_CTYPE", "en_US.utf-8")
 #Run with source("/home/luised94/lab_utils/next_generation_sequencing/003_fastqProcessing/006_R_node_readInFastqc.R")
 setwd("/net/bmc-pub14/data/bell/users/luised94/240304Bel")
-df_sample_info <- read.delim("./documentation/sample_info_table.csv", header = TRUE, sep = ",")
-sample_ids <- df_sample_info$sample_ID
-base_dir <- "./qualityControl"
+#df_sample_info <- read.delim("./documentation/sample_info_table.csv", header = TRUE, sep = ",")
+#sample_ids <- df_sample_info$sample_ID
+#base_dir <- "./qualityControl"
 
-qualityControl_dir <- list.dirs("./qualityControl", recursive = FALSE)
-contains_sample_id <- grepl(sample_ids[1], list.dirs("./qualityControl", recursive = FALSE))
-subset_qualityControl_dir <- qualityControl_dir[contains_sample_id][1]
-fastqc_file_path <- list.files(subset_qualityControl_dir, pattern = "fastqc_data", full.names = TRUE)	
-summary_file_path <- list.files(subset_qualityControl_dir, pattern = "summary", full.names = TRUE)	
+#qualityControl_dir <- list.dirs("./qualityControl", recursive = FALSE)
+#contains_sample_id <- grepl(sample_ids[1], list.dirs("./qualityControl", recursive = FALSE))
+#subset_qualityControl_dir <- qualityControl_dir[contains_sample_id][1]
+fastqc_file_path <- list.files("./qualityControl", pattern = "fastqc_data", recursive = TRUE, full.names = TRUE)	
+summary_file_path <- list.files("./qualityControl", pattern = "summary", recursive = TRUE, full.names = TRUE)	
+head(fastqc_file_path)
+head(summary_file_path)
 
-data_blocks <- list()
-con <- file(fastqc_file_path, open = "r")
-counter <- 0
-
-
-while (TRUE) {
-  message <- sprintf("Loop counter: %s", counter)
-  print(message)
-  line <- readLines(con, n = 1, warn = FALSE)
-#  line_content <- sprintf("Content of line is: %s", line)
-#  print(line_content)
-
-  if (length(line) == 0) { 
-	print("Line is length 0, Exiting")
-	break
-  }
-  
-  if (grepl(">>", line)) {
-    block_name <- sub(">> ", "", line)
-    data_chunk <- ""
-#    if (counter 
-    message <- sprintf("Block name is %s", block_name)
-    print(message)
-    while (TRUE) {
-      line <- readLines(con, n = 1)
-      if (grepl(">>END_MODULE", line)) { 
-	message <- "Reached end of module"
-        print(message)
-	break 
-	}
-      data_chunk <- paste(data_chunk, line, sep = "\n")
-      print(data_chunk)
-    }
-    data_blocks[[block_name]] <- data_chunk
-  }
-  
-  counter <- counter + 1
+max_index <- 1
+for (i in 1:max_index) {
+	lines <- readLines(fastqc_file_path[i])
+	head(lines)
 }
 
-close(con)
-head(data_blocks)
+#
+#data_blocks <- list()
+#con <- file(fastqc_file_path, open = "r")
+#counter <- 0
+#
+#
+#while (TRUE) {
 #  message <- sprintf("Loop counter: %s", counter)
 #  print(message)
 #  line <- readLines(con, n = 1, warn = FALSE)
-#  line_content <- sprintf("Content of line is: %s", line)
-#  print(line_content)
+##  line_content <- sprintf("Content of line is: %s", line)
+##  print(line_content)
+#
 #  if (length(line) == 0) { 
 #	print("Line is length 0, Exiting")
 #	break
 #  }
+#  
 #  if (grepl(">>", line)) {
 #    block_name <- sub(">> ", "", line)
 #    data_chunk <- ""
+##    if (counter 
 #    message <- sprintf("Block name is %s", block_name)
 #    print(message)
 #    while (TRUE) {
@@ -77,7 +55,39 @@ head(data_blocks)
 #    }
 #    data_blocks[[block_name]] <- data_chunk
 #  }
+#  
 #  counter <- counter + 1
-parse_summary <- function(file_path) {
-#TODO after working out logic of reading summary file, convert into function, add more descriptive name and Roxygen2 style documentation
-}
+#}
+#
+#close(con)
+#head(data_blocks)
+##  message <- sprintf("Loop counter: %s", counter)
+##  print(message)
+##  line <- readLines(con, n = 1, warn = FALSE)
+##  line_content <- sprintf("Content of line is: %s", line)
+##  print(line_content)
+##  if (length(line) == 0) { 
+##	print("Line is length 0, Exiting")
+##	break
+##  }
+##  if (grepl(">>", line)) {
+##    block_name <- sub(">> ", "", line)
+##    data_chunk <- ""
+##    message <- sprintf("Block name is %s", block_name)
+##    print(message)
+##    while (TRUE) {
+##      line <- readLines(con, n = 1)
+##      if (grepl(">>END_MODULE", line)) { 
+##	message <- "Reached end of module"
+##        print(message)
+##	break 
+##	}
+##      data_chunk <- paste(data_chunk, line, sep = "\n")
+##      print(data_chunk)
+##    }
+##    data_blocks[[block_name]] <- data_chunk
+##  }
+##  counter <- counter + 1
+#parse_summary <- function(file_path) {
+##TODO after working out logic of reading summary file, convert into function, add more descriptive name and Roxygen2 style documentation
+#}
