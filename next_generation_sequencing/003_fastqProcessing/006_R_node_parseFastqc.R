6e70108e925b6a87ccb770137caefd83e7f3c4ac
Luis <luised94@mit.edu>
2024-05-14 12:39:25 -0400

    Moved generateCoverageFiles.sh to third position. Added handling of no argument provided or no directory exists to parseFastqc and checkQcBam

--- next_generation_sequencing/003_fastqProcessing/006_R_node_parseFastqc.R
+++ next_generation_sequencing/003_fastqProcessing/006_R_node_parseFastqc.R
@@ -1,26 +1,29 @@
 #Run with: Rscript lab_utils/next_generation_sequencing/003_fastqProcessing/006_R_node_parseFastqc.R dirname > output.log 2>&1
 args <- commandArgs(trailingOnly = TRUE)
+if (length(args) == 0 ) { print("No argument provided. Provide a directory to process") ; q() }
 
 get_current_datetime_string <- function() {
                   return(format(Sys.time(), "%Y-%m-%d-%H-%M-%S"))
 }
 
 find_the_directory <- function(dirname){
-	data_directory <- paste0(Sys.getenv("HOME"), "/data")
-	contains_dirname <- grepl(dirname, list.dirs(data_directory, recursive = FALSE))
-	directory_path <- list.dirs(data_directory, recursive = FALSE)[contains_dirname]
-	if (dir.exists(directory_path)) { return(directory_path) } 
-	else { paste0("Directory ", dirname, " not found") ; exit() }
+    data_directory <- paste0(Sys.getenv("HOME"), "/data")
+    contains_dirname <- grepl(dirname, list.dirs(data_directory, recursive = FALSE))
+    directory_path <- list.dirs(data_directory, recursive = FALSE)[contains_dirname]
+    if (length(directory_path) > 0 && dir.exists(directory_path)) { return(directory_path) }
+    else { print(paste0("Directory ", dirname, " not found")) ; return(NULL) }
 }
 
 directory_to_process <- find_the_directory(args[1])
+if (is.null(directory_to_process)) { print("Directory to process is null. Exiting script.") ; q() }
+
 message <- sprintf("Directory to process is %s.", directory_to_process)
 print(message)
 setwd(directory_to_process)
 
-fastqc_file_path <- list.files("./qualityControl", pattern = "fastqc_data", recursive = TRUE, full.names = TRUE)	
+fastqc_file_path <- list.files("./qualityControl", pattern = "fastqc_data", recursive = TRUE, full.names = TRUE)
 print(fastqc_file_path)
-print(paste0("Number of files to process ", length(fastqc_file_path))
+print(paste0("Number of files to process ", length(fastqc_file_path)))
 
 number_of_files <- length(fastqc_file_path)
 for (file_idx in 1:number_of_files) {
