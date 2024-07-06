38d78572baf14059a87828d12d4fc9ab2bfb793b
luised94 <luised94@mit.edu>
2024-07-06 01:09:04 -0400

    Opens in neovim and can navigate fairly well Added sed to process certain metadata automatically.

--- experiment_management/create_new_experiment.sh
+++ experiment_management/create_new_experiment.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+#determine_experiment_id() {}
+
 create_new_experiment() {
     local date_of_creation=$(date +%Y%m%d)
     local filename="${date_of_creation}_experiment.md"
     SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-
+    template_file="${SCRIPT_DIR}/templates/experiment_template.md" 
+    sed "s/{{EXPERIMENT_NAME}}/trial/g; s/{{DATE}}/${date_of_creation}/g" "$template_file" > "$filename"
+    nvim $filename
 }
 create_new_experiment
