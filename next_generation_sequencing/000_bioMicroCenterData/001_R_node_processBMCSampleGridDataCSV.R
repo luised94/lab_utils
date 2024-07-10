65a1d3931826f50896cc3d00c9a5b45fd812927e
luised94 <luised94@mit.edu>
2024-07-10 12:32:15 -0400

    Removed tabs from script, added documentation to manual, added some TODOs to processBMCSampleGridDataCSV.R

--- next_generation_sequencing/000_bioMicroCenterData/001_R_node_processBMCSampleGridDataCSV.R
+++ next_generation_sequencing/000_bioMicroCenterData/001_R_node_processBMCSampleGridDataCSV.R
@@ -1,21 +1,24 @@
 #PURPOSE: Process the BMC sample grid data to create a csv file with the sample number, information and short name for further processing
 #USAGE: Run as a script with Rscript and positional arguments or replace commented directory to scan line. 
+#Example: Rscript ./001_R_node_processBMCSampleGridDataCSV.R <directory_to_process>
 #DEPENDENCY: Assumes directory structure of directory_creation.sh and requires the BMC_sample_grid_data_*.csv file. 
+#TODO: How to deal with multiple BMC files. Maybe just have it as separate directory for each sequencing run. Maybe will neeed if I do a bunch of samples.
+#TODO: Need to integrate with script that creates the grid in the first place which is not in the repository currently. Need to provide as argument for the sample_ID and access with args[2] then use nrow to create the last number in the seq function.
 args <- commandArgs(trailingOnly = TRUE)
 
 directory_to_scan <- paste(Sys.getenv("HOME"), "data", args[1], "documentation", sep = "/")
 
 #directory_to_scan <- paste(Sys.getenv("HOME"), "data", "240304Bel", "documentation", sep = "/")
-# TODO: How to deal with multiple BMC files. Maybe just have it as separate directory for each sequencing run. Maybe will neeed if I do a bunch of samples.
+
 sample_grid_data_paths <- list.files(path = directory_to_scan, pattern = "BMC")
 
 sample_info_string_names <- read.csv(sample_grid_data_paths)[,1]
 
 sample_info_table <- as.data.frame(
-				   do.call(rbind,
-	                                   stringi::stri_split_regex(sample_info_string_names, pattern = "_")
-                                   )
-                     ) 
+    do.call(rbind,
+    stringi::stri_split_regex(sample_info_string_names, pattern = "_")
+    )
+)
 
 
 
@@ -25,14 +28,13 @@ colnames(sample_info_table) <- c("ORC4_Rescue", "Suppressor", "Cell_Cycle", "Aux
 sample_info_table$sample_ID <- seq(148001, 148049, by = 1)
 
 #Take the first string of the first five columns to create the short name column. Easier for filtering the columns. 
-sample_info_table$short_name <- apply(sample_info_table[, 1:5], 
-				      1, 
-			              function(x) paste0(substr(x, 1,1), collapse = "")
-				)
+sample_info_table$short_name <- apply(
+    sample_info_table[, 1:5], 
+    1, 
+    function(x) paste0(substr(x, 1,1), collapse = "")
+)
 
 write.csv(sample_info_table, 
-	  paste(directory_to_scan, "sample_info_table.csv", sep="/"), 
-	  row.names = FALSE
-	 )
-
-
+    paste(directory_to_scan, "sample_info_table.csv", sep="/"), 
+    row.names = FALSE
+)
