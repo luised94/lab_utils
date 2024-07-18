0d20e73300582edb1d67c3e6033ad7c307c3a760
Luis <luised94@mit.edu>
2024-07-18 17:08:58 -0400

    Added comment to usage line to use one line at a time. Modified the find and rm commands but this is not too important

--- next_generation_sequencing/generalSetup/001_downloadDataFromBMC.sh
+++ next_generation_sequencing/generalSetup/001_downloadDataFromBMC.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #DESCRIPTION: Download the fastq file from the BMC servers. Ignore bams, htmls, fastqc etc. Only the BMC processed fastq pipeline.
-#USAGE:
+#USAGE: Still working one turning it into a robust  script but rm -rf is dangerous. 
 
 bmc_server=$1
 bell_lab_directory=$2
 srun rsync -av -n --include '*/' --include '*.fastq' --exclude '*' /net/${bmc_server}/data/bmc/public/Bell/${bell_lab_directory}/ ~/data/${bell_lab_directory}/fastq/
 cd ~/data/${bell_lab_directory}/fastq/
 find . -type f -name "*.fastq" -exec mv {} . \;
-find . -type d \( -name "*D24*" -o -name "infosite*" \) -exec rm -rf {} \;
-rm ~/data/${bell_lab_directory}/fastq/*unmapped*.fastq
+find . -type d \( -name "*D24*" -o -name "infosite*" \) -exec rm -rf {} +
+rm *unmapped*.fastq
