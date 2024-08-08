e1b98747fa9818da9c339a4160f8dfb4bfde1f04
luised94 <luised94@mit.edu>
2024-08-07 22:42:12 -0400

    Created the sample_table using lapply and ls functions to go through the is_ containing variables. Create the bmc table. Write both to files.

--- next_generation_sequencing/000_generalSetup/002_createSampleGrid.R
+++ next_generation_sequencing/000_generalSetup/002_createSampleGrid.R
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 734f1db (Started createSampleGrid.R)
 rm(list = ls())
 category_names <- c("rescue_allele", "mcm_tag", "auxin_treatment", "cell_cycle", "antibody")
 
 strain_source <- c("lemr", "oa")
 rescue_allele <- c("none", "wt")
-<<<<<<< HEAD
 mcm_tag <- c("none", "2", "4", "7")
-=======
 mcm_tag <- c("none", "2", "7")
->>>>>>> 734f1db (Started createSampleGrid.R)
 auxin_treatment <- c("no", "yes")
 cell_cycle <- c("G1", "M")
 antibody <- c("Input", "ProtG", "ALFA", "HM1108", "74", "CHA", "11HA")
@@ -31,11 +24,9 @@ is_input <- sample_combinations[, "rescue_allele"] == "none" &
             ((sample_combinations[, "strain_source"] == "oa" & sample_combinations[, "auxin_treatment"] == "no") | (sample_combinations[, "strain_source"] == "lemr"))
 
 print(sum(is_input))
-<<<<<<< HEAD
 print(sample_combinations[is_input,])
 
 
-=======
 #print(sample_combinations[is_input,])
 
 
@@ -87,4 +78,21 @@ is_11HA <- sample_combinations[, "antibody"] == "11HA" &
           !(sample_combinations[, "strain_source"] == "oa" & sample_combinations[, "mcm_tag"] == "2")
 print(sum(is_11HA))
 print(sample_combinations[is_11HA,])
->>>>>>> 734f1db (Started createSampleGrid.R)
+
+subset_vectors <- ls()[grepl("is_", ls())]
+sample_table <- do.call(rbind, lapply(subset_vectors, function(subset_vector){
+    sample_combinations[get(subset_vector), ]
+}))
+print(sample_table)
+print(dim(sample_table))
+
+bmc_table <- data.frame(SampleName = sample_table$full_name,
+    Vol..uL = rep(10, nrow(sample_table)),
+    Conc = rep(NA, nrow(sample_table)),
+    Type = rep("ChIP", nrow(sample_table)),
+    Genome = rep("Saccharomyces cerevisiae", nrow(sample_table)),
+    Notes = rep("none", nrow(sample_table)),
+    Pool = rep("A", nrow(sample_table))
+)
+write.table(sample_table, file = "sample_info.tsv", sep = "\t", row.names = FALSE)
+write.table(bmc_table, file = "bmc_info.tsv", sep = "\t", row.names = FALSE)
