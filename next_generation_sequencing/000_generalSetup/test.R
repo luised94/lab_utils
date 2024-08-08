32f979aaf5d73a8f6243a9a8bcaedebed52299db
luised94 <luised94@mit.edu>
2024-08-08 17:13:31 -0400

    Move the lines from test.R to 000_createSampleGrid.R since they seem to be working now

--- next_generation_sequencing/000_generalSetup/test.R
+++ next_generation_sequencing/000_generalSetup/test.R
@@ -4,5 +4,7 @@ args <- commandArgs(trailingOnly = TRUE)
 experiment_dir <- paste(dropbox_dir, args[1], sep = "")
 print(experiment_dir)
 dir.create(experiment_dir)
-"mkdir -p"
-system("mkdir -p""$dir"/{peak,fastq,alignment,qualityControl,bigwig,plots,logs,documentation}
+dir_creation_command <- paste("mkdir -p '", experiment_dir, "'/{peak,fastq,alignment,qualityControl,bigwig,plots,logs,documentation}", sep = "")
+print(dir_creation_command)
+cat("Command:", dir_creation_command, "\n")
+#system(dir_creation_command)
