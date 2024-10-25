fd400fa63321c3030c4bbfa205040661008fa218
luised94 <luised94@mit.edu>
2024-10-25 15:53:18 -0400

    Remove redundancy from init_config.R

--- R/config/init_config.R
+++ R/config/init_config.R
@@ -1,55 +1,12 @@
-
-#' Initialization Configuration
-CONFIG <- list(
-    INITIALIZATION = list(
-        PATHS = list(
-            BASE = "R",
-            FUNCTIONS = "functions",
-            SCRIPTS = "scripts",
-            CONFIG = "config",
-            TEMPLATES = "templates"
-        ),
-        
-        PATTERNS = list(
-            R_FILES = "\\.R$",
-            CONFIG_FILES = "^[^.].*\\.R$",
-            EXCLUDE = c("^\\.", "^_", "test_", "example_")
-        ),
-        
-        LOAD_ORDER = list(
-            PRIORITY = c(
-                "logging_utils.R",
-                "environment_utils.R",
-                "validation_utils.R"
-            ),
-            OPTIONAL = c(
-                "analysis_utils.R",
-                "visualization_utils.R"
-            )
-        )
-    ),
-    
-    ENVIRONMENT = list(
-        REQUIRED_VARS = c(
-            "HOME",
-            "R_LIBS_USER",
-            "WINDOWS_USER"
-        ),
-        
-        PATHS = list(
-            USER_LIB = "~/R/library",
-            RENV = "renv"
-        )
-    )
-)
-#' Initialization Configuration
+# init_config.R
 CONFIG <- list(
     PATHS = list(
-        BASE = "R",
+        BASE = Sys.getenv("R_PROJECT_ROOT", "R"),
         FUNCTIONS = "functions",
         SCRIPTS = "scripts",
         CONFIG = "config",
-        TEMPLATES = "templates"
+        TEMPLATES = "templates",
+        LOGS = "logs"
     ),
     
     LOAD_ORDER = list(
@@ -57,10 +14,6 @@ CONFIG <- list(
             "logging_utils.R",
             "environment_utils.R",
             "validation_utils.R"
-        ),
-        OPTIONAL = c(
-            "analysis_utils.R",
-            "visualization_utils.R"
         )
     ),
     
@@ -73,7 +26,13 @@ CONFIG <- list(
         REQUIRED_VARS = c(
             "HOME",
             "R_LIBS_USER",
-            "WINDOWS_USER"
+            "R_PROJECT_ROOT"
         )
+    ),
+    
+    CLUSTER = list(
+        ENABLED = as.logical(Sys.getenv("R_CLUSTER_MODE", "FALSE")),
+        MOUNT_POINT = Sys.getenv("R_CLUSTER_MOUNT", "/cluster/data")
     )
 )
+cat(CONFIG$PATHS$BASE)
