7095ab677471680f8132897827f75095359b7022
luised94 <luised94@mit.edu>
2024-10-29 11:30:46 -0400

    Created the bmc_sample_grid_config_template.R and updated the bmc_sample_grid_config.R to the latest bmc experiment, 241007Bel.

--- R/config/bmc_sample_grid_config_template.R
+++ R/config/bmc_sample_grid_config_template.R
@@ -7,57 +7,56 @@
 #' @description Define experimental setup and validation
 #' @export EXPERIMENT_CONFIG
 
-
 EXPERIMENT_CONFIG <- list(
     METADATA = list(
-        EXPERIMENT_ID = "241007Bel",
-        EXPECTED_SAMPLES = 65,
+        EXPERIMENT_ID = "240808Bel",
+        EXPECTED_SAMPLES = 33,
         VERSION = "1.0.0"
     ),
     
     CATEGORIES = list(
-        rescue_allele = c("NONE", "WT", "4R", "PS"),
-        auxin_treatment = c("NO", "YES"),
-        time_after_release = c("0", "1", "2"),
-        antibody = c("Input", "ProtG", "HM1108", "V5", "ALFA", "UM174")
+        strain_source = c("lemr", "oa"),
+        rescue_allele = c("none", "wt"),
+        mcm_tag = c("none", "2", "7"),
+        auxin_treatment = c("no", "yes"),
+        cell_cycle = c("G1", "M"),
+        antibody = c("Input", "ProtG", "ALFA", "HM1108", "74", "CHA", "11HA")
     ),
     
     INVALID_COMBINATIONS = list(
-        rescue_allele_auxin_treatment = quote(rescue_allele %in% c("4R", "PS") & auxin_treatment == "NO"),
-        protg_time_after_release = quote(antibody == "ProtG" & time_after_release %in% c("1", "2")),
-        input_time_after_release = quote(antibody == "Input" & time_after_release %in% c("1", "2")),
-        input_rescue_allele_auxin_treatment = quote(antibody == "Input" & rescue_allele %in% c("NONE", "WT") & auxin_treatment == "YES")
+        lemr_mcm7 = quote(strain_source == "lemr" & mcm_tag == "7"),
+        lemr_mcm2 = quote(strain_source == "lemr" & mcm_tag == "2"),
+        oa_mcm2 = quote(strain_source == "oa" & mcm_tag == "2"),
+        oa_wt = quote(strain_source == "oa" & rescue_allele == "wt")
     ),
     
     EXPERIMENTAL_CONDITIONS = list(
-        is_input = quote(time_after_release == "0" & antibody == "Input"),
-        is_protg = quote(rescue_allele == "WT" & time_after_release == "0" & antibody == "ProtG" & auxin_treatment == "NO"),
-        is_v5 = quote(antibody == "V5"),
-        is_alfa = quote(antibody == "ALFA"),
-        is_1108 = quote(antibody == "HM1108" & time_after_release == "0"),
-        is_174 = quote(antibody == "UM174")
+        is_input = quote(rescue_allele == "none" & cell_cycle == "M" & 
+                        antibody == "Input" & 
+                        ((strain_source == "oa" & auxin_treatment == "no") | 
+                         (strain_source == "lemr" & auxin_treatment == "no"))),
+        is_protg = quote(rescue_allele == "wt" & mcm_tag == "none" & 
+                        cell_cycle == "M" & antibody == "ProtG" & 
+                        strain_source == "lemr" & auxin_treatment == "no")
+        # [Additional conditions...]
     ),
     
     COMPARISONS = list(
-        comp_1108forNoneAndWT = quote(antibody == "HM1108" & rescue_allele %in% c("NONE", "WT")),
-        comp_1108forNoneAndWT_auxin = quote(antibody == "HM1108" & auxin_treatment == "YES"),
-        comp_timeAfterReleaseV5WT = quote(antibody == "V5" & rescue_allele == "WT" & auxin_treatment == "YES"),
-        comp_timeAfterReleaseV5NoTag = quote(antibody == "V5" & rescue_allele == "NONE" & auxin_treatment == "YES"),
-        comp_V5atTwoHours = quote(antibody == "V5" & time_after_release == "2" & auxin_treatment == "YES"),
-        comp_UM174atTwoHours = quote(antibody == "UM174" & time_after_release == "2" & auxin_treatment == "YES"),
-        comp_ALFAforNoRescueNoTreat = quote(antibody == "ALFA" & rescue_allele == "NONE" & auxin_treatment == "NO"),
-        comp_ALFAforNoRescueWithTreat = quote(antibody == "ALFA" & rescue_allele == "NONE" & auxin_treatment == "YES"),
-        comp_ALFAatTwoHoursForAllAlleles = quote(antibody == "ALFA" & time_after_release == "2" & auxin_treatment == "YES"),
-        comp_UM174atZeroHoursForAllAlleles = quote(antibody == "UM174" & time_after_release == "0" & auxin_treatment == "YES"),
-        comp_AuxinEffectOnUM174 = quote(antibody == "UM174" & time_after_release == "2" & rescue_allele %in% c("NONE", "WT"))
+        comp_cellCycle = quote(strain_source == "lemr" & antibody == "74"),
+        comp_alfa = quote(antibody == "ALFA")
+        # [Additional comparisons...]
     ),
     
     CONTROL_FACTORS = list(
-        genotype = c("rescue_allele")
+        genotype = c("strain_source", "rescue_allele", "mcm_tag")
     ),
     
-    COLUMN_ORDER = c("antibody", "rescue_allele", "auxin_treatment", "time_after_release")
+    COLUMN_ORDER = c(
+        "antibody", "strain_source", "rescue_allele", 
+        "mcm_tag", "auxin_treatment", "cell_cycle"
+    )
 )
+
 
 #' Validate Category Values
 validate_experiment_categories <- function(experiment_config) {
