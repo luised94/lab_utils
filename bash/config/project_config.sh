be7ed42b2cd0b5ffd3ddd09c1d6ec90eb0cf2d49
luised94 <luised94@mit.edu>
2024-10-29 14:31:17 -0400

    Remove logs from required folders. New logging_utils are centralized.

--- bash/config/project_config.sh
+++ bash/config/project_config.sh
@@ -5,7 +5,7 @@ declare -A PROJECT_CONFIG=(
     [REMOTE_HOST]="luria.mit.edu"
     [REMOTE_USER]="luised94"
     [REMOTE_PATH]="~/data"
-    [REQUIRED_DIRS]="documentation fastq logs"
+    [REQUIRED_DIRS]="documentation fastq"
     [DEFAULT_LOG_ROOT]="$HOME/logs"
     [LOG_LEVELS]="TRACE DEBUG INFO WARNING ERROR FATAL"
 )
