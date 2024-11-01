12743943054c0b40e44ffba641ea433719aedf1b
luised94 <luised94@mit.edu>
2024-11-01 03:28:41 -0400

    add lock settings to logging config.

--- bash/config/logging_config.sh
+++ bash/config/logging_config.sh
@@ -6,10 +6,21 @@ declare -A LOGGING_CONFIG=(
     [TIMESTAMP_FORMAT]="%Y-%m-%d %H:%M:%S"
     [ENTRY_FORMAT]="\n%s (#%d) === %s ===\n"
     [FIRST_RUN_FORMAT]="%s (#1) === %s ==="
-    [LOCK_TIMEOUT]="10"           # Seconds to wait for lock
-    [LOCK_RETRY]="3"             # Number of retry attempts
     [BUFFER_SIZE]="4096"         # Write buffer size
+    [LOCK_BASE_DIR]="/tmp/lab_utils_locks"
+    [LOCK_TIMEOUT]=10
+    [LOCK_RETRY]=3
     [MAX_MESSAGE_LENGTH]="1024"   # Maximum message length
     [DEFAULT_LOG_ROOT]="$HOME/logs"
     [LOG_LEVELS]="TRACE DEBUG INFO WARNING ERROR FATAL"
+)
+
+
+# Critical path protection
+declare -rA PROTECTED_PATHS=(
+    ["/bin"]=1
+    ["/etc"]=1
+    ["/dev"]=1
+    ["/home"]=1
+    ["$HOME"]=1
 )
