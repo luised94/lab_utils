1cfa27dc7a1c14884db24cd530856b2ec59cfb86
luised94 <luised94@mit.edu>
2024-11-01 21:04:41 -0400

    Adjust lock file creation to lius directory.

--- bash/core/logging.sh
+++ bash/core/logging.sh
@@ -42,7 +42,7 @@ write_log_atomic() {
     local entry="$1"
     local log_file="$2"
     local lock_name="log_$(basename "$log_file")"
-    local lock_file="${CORE_CONFIG[LOCK_BASE_DIR]}/${lock_name}.lock"
+    local lock_file="${CORE_CONFIG[LOCK_BASE_DIR]}/${USER}/${lock_name}.lock"
     local retry_count=0
 
     # Ensure lock directory exists
