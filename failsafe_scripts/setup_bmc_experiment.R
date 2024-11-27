556c641ef73f8941089eec9ba9df6424968c5992
luised94 <luised94@mit.edu>
2024-11-27 14:47:34 -0500

    Consolidate and remove duplicate and empty files.

--- failsafe_scripts/setup_bmc_experiment.R
+++ failsafe_scripts/setup_bmc_experiment.R
@@ -1,4 +1,34 @@
 
+#' Add to existing configurations
+CONFIG <- list(
+    EXPERIMENT = list(
+        PATHS = list(
+            DROPBOX = "/mnt/c/Users/%s/Dropbox (MIT)/",
+            CONFIG = "sampleGridConfig.R"
+        ),
+        
+        DIRECTORIES = c(
+            "peak",
+            "fastq",
+            "alignment",
+            "qualityControl",
+            "bigwig",
+            "plots",
+            "documentation"
+        ),
+        
+        OUTPUT = list(
+            ENABLED = FALSE,
+            CONFIG_TEMPLATE = "%s_%s.R"
+        )
+    ),
+    
+    ENVIRONMENT = list(
+        REQUIRED_VARS = c(
+            "WINDOWS_USER"
+        )
+    )
+)
 CONFIG <- list(
     ID_REGEX = "\\d{5,6}",
     FILE_PATTERNS = list(
