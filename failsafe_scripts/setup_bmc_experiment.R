af40fed1afa255a46bf5ae25452e9eb8c5a97d52
luised94 <luised94@mit.edu>
2024-11-27 20:04:14 -0500

    Go with simple data_directories array. Add verification for Bel experiment id. Remove function to create directories and convert to procedural instruction.

--- failsafe_scripts/setup_bmc_experiment.R
+++ failsafe_scripts/setup_bmc_experiment.R
@@ -1,4 +1,3 @@
-
 #' Add to existing configurations
 CONFIG <- list(
     EXPERIMENT = list(
@@ -51,17 +50,26 @@ DEBUG_CONFIG <- list(
     interactive = FALSE,
     display_time = 2
 )
-create_experiment_dir <- function(directory_path, subdirectories){
-    if (!dir.exists(directory_path)) {
-        dir.create(directory_path, recursive = TRUE)
-    } else {
-        cat(sprintf("Directory %s already exists.\n", directory_path))
-    }
-    sapply(file.path(directory_path, subdirectories), function(path_to_create) {
-        if (!dir.exists(path_to_create)) {
-            dir.create(path_to_create)
-        } else {
-            cat(sprintf("Directory %s already exists.\n", path_to_create))
-        }
-    })
+
+experiment_id <- "241122Bel"
+if (!grepl("^\\d{6}Bel$", experiment_id)) {
+    stop("Invalid experiment ID format. Expected: YYMMDD'Bel'")
 }
+
+base_dir <- file.path(Sys.getenv("HOME"), "data", experiment_id)
+
+# Define the paths directly in a character vector
+data_directories <- c(
+    "peak",
+    "fastq/raw",
+    "fastq/processed",
+    "alignment",
+    "bigwig",
+    "plots/genome_tracks/overview",
+    "plots/genome_tracks/experimental_comparisons",
+    "documentation/dna_qc_traces",
+)
+
+full_paths <- file.path(base_dir, data_directories)
+sapply(full_paths, dir.create, recursive = TRUE, showWarnings = FALSE)
+cat("Directories created successfully!\n")
