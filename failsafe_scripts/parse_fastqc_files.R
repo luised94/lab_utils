fed5137bd075283ba83dd4d8e8d2796a77b73a0a
luised94 <luised94@mit.edu>
2024-12-02 17:01:26 -0500

    Add stopifnot statements for directories and files found. Add logic for fastqc processing.

--- failsafe_scripts/parse_fastqc_files.R
+++ failsafe_scripts/parse_fastqc_files.R
@@ -1,6 +1,5 @@
-
 ################################################################################
-# BMC Experiment Configuration
+# Parse fastqc files for an experiment.
 ################################################################################
 # PURPOSE:
 #
@@ -27,17 +26,18 @@
 ################################################################################
 # !! Review debug configuration
 DEBUG_CONFIG <- list(
-    enabled = FALSE,
+    enabled = TRUE,
     verbose = TRUE,
     interactive = TRUE,
-    dry_run = FALSE
+    dry_run = FALSE,
+    files_to_process_idx = 1
 )
 
 FASTQC_CONFIG <- list(
     VERSION = "1.0.0",
     FASTQC_DATA_PATTERN = "fastqc_data",
     OUTPUT_SUFFIX = ".tab",
-    QC_SUBDIR = "quality_control",
+    QC_SUBDIR = "quality_control"
 )
 
 #CONFIG$COLUMN_NAMES <- list(
@@ -58,14 +58,85 @@ TIMESTAMPS <- list(
 
 
 # !! Update experiment_id to denote the data directory that will be processed.
-experiment_id <- "241122Bel"
+experiment_id <- "241007Bel"
 base_experiment_dir <- file.path(Sys.getenv("HOME"), "data", experiment_id)
-if (is.null(base_experiment_dir) || !dir.exists(base_experiment_dir)) {
-    stop(sprintf("Invalid directory path: %s", base_experiment_dir))
+stopifnot(
+    "Base experiment not set." = !is.null(base_experiment_dir),
+    "Directory does not exist." = dir.exists(base_experiment_dir)
+)
+
+##########
+quality_control_dir <- file.path(base_experiment_dir, FASTQC_CONFIG$QC_SUBDIR)
+stopifnot(
+    "Quality control directory does not exist." = dir.exists(quality_control_dir)
+)
+
+fastqc_files <- list.files(
+    quality_control_dir,
+    pattern = FASTQC_CONFIG$FASTQC_DATA_PATTERN,
+    recursive = TRUE,
+    full.names = TRUE
+)
+
+stopifnot(
+    "No quality control files found in experiment directory." = length(fastqc_files) > 0
+)
+
+files_to_process_indexes <- if (DEBUG_CONFIG$enabled) {
+    DEBUG_CONFIG$files_to_process_idx
+} else {
+    seq_along(fastqc_files)
 }
 
-base::sprintf("%s_%s_%s%s",
-    get_timestamp(),
-    prefix,
-    module_name,
-    FASTQC_CONFIG$OUTPUT_SUFFIX)
+for (sample_idx in files_to_process_indexes) {
+    if (DEBUG_CONFIG$verbose) {
+        message("\nProcessing sample ", sample_idx)
+    }
+
+    lines <- readLines(fastqc_files[sample_idx])
+    modules_starts <- which(grepl(">>", lines))
+    modules_ends <- which(grepl(">>END_MODULE", lines))
+    modules_starts <- modules_starts[!(modules_starts %in% modules_ends)]
+    output_dir <- dirname(fastqc_files[file_idx])
+    fastqc_summary <- list()
+    for (module_idx in seq_along(modules_starts)) {
+        module_lines <- lines[modules_starts[module_idx]:modules_ends[module_idx]]
+        module_summary <- gsub(">>", "", module_lines[1])
+        fastqc_summary <- append(fastqc_summary, module_summary)
+        module_filename <- gsub(" ", "", strsplit(module_summary, "\t")[[1]][1])
+        potential_headers <- which(grepl("^#", module_lines[2:length(module_lines)-1]))
+        last_potential_header <- potential_headers[length(potential_headers)] 
+        for (header_idx in potential_headers) {
+            number_of_elements_in_header <- length(strsplit(module_lines[header_idx], "\t")[[1]])
+            number_of_elements_in_line <- length(strsplit(module_lines[last_potential_header], "\t")[[1]])
+            if(number_of_elements_in_header == number_of_elements_in_line) {
+                header <- gsub("#", "", module_lines[header_idx])
+                if (DEBUG_CONFIG$dry_run) {
+                    data <- read.table(text = module_lines[(header_idx+1):length(module_lines)-1],
+                                   header = FALSE,
+                                   col.names = strsplit(header, "\t")[[1]],
+                                   sep = "\t")
+                }
+            }
+
+        }
+        output_file_name <- paste0(current_time, "_", "fastqc_", module_filename, ".tab")
+        output_file_path <- file.path(output_dir, output_file_name)
+        write.table(data, file = output_file_path, append = FALSE, quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
+    }
+    fastqc_summary <- read.table(text = unlist(fastqc_summary),
+                       header = FALSE,
+                       col.names = c("Stat", "Value"),
+                       sep = "\t")
+    print(head(fastqc_summary))
+    output_file_name <- paste0(current_time, "_", "fastqc_", "summary", ".tab")
+    output_file_path <- file.path(output_dir, output_file_name)
+    write.table(fastqc_summary, file = output_file_path, append = FALSE, quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
+}
+
+#base::sprintf("%s_%s_%s%s",
+#    get_timestamp(),
+#    prefix,
+#    module_name,
+#    FASTQC_CONFIG$OUTPUT_SUFFIX)
+#
