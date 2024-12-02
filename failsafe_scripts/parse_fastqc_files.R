bb9748a2f7c9c0d5fd659de140faac7101dc34a5
luised94 <luised94@mit.edu>
2024-12-02 14:49:53 -0500

    Add HEADER COMMENT to fastqc file.

--- failsafe_scripts/parse_fastqc_files.R
+++ failsafe_scripts/parse_fastqc_files.R
@@ -1,3 +1,27 @@
+
+################################################################################
+# BMC Experiment Configuration
+################################################################################
+# PURPOSE:
+#
+# USAGE:
+#   1. Use '/!!' in vim/neovim to jump to required updates
+#
+# !! ----> REQUIRED UPDATES:
+#
+# STRUCTURE:
+#
+# VALIDATION:
+#
+# DEPENDENCIES:
+#
+# COMMON ISSUES:
+#
+# AUTHOR: Luis
+# DATE: 2024-12-02
+# VERSION: 1.0.0
+#
+################################################################################
 ################################################################################
 # Configuration and Debug Settings
 ################################################################################
@@ -33,15 +57,15 @@ TIMESTAMPS <- list(
 )
 
 
+# !! Update experiment_id to denote the data directory that will be processed.
 experiment_id <- "241122Bel"
 base_experiment_dir <- file.path(Sys.getenv("HOME"), "data", experiment_id)
-if (is.null(dir_path) || !dir.exists(base_experiment_dir)) {
+if (is.null(base_experiment_dir) || !dir.exists(base_experiment_dir)) {
     stop(sprintf("Invalid directory path: %s", base_experiment_dir))
 }
+
 base::sprintf("%s_%s_%s%s",
     get_timestamp(),
     prefix,
     module_name,
     FASTQC_CONFIG$OUTPUT_SUFFIX)
-
-
