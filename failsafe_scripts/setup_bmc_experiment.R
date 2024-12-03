872fb2a49e7b87d08410251e3ca93909c94ffe14
luised94 <luised94@mit.edu>
2024-12-03 15:05:19 -0500

    Add SAMPLE_CLASSIFICATIONS list to EXPERIMENT_CONFIG. This will serve as an additional factor variable for quality control analysis. Remove commented code at the bottom. Not required.

--- failsafe_scripts/setup_bmc_experiment.R
+++ failsafe_scripts/setup_bmc_experiment.R
@@ -61,7 +61,6 @@
 # VERSION: 2.0.0
 #
 ################################################################################
-
 ################################################################################
 # Configuration and Debug Settings
 ################################################################################
@@ -287,46 +286,3 @@ if (DEBUG_CONFIG$dry_run) {
         }
     }
 }
-
-##' Add to existing configurations
-#CONFIG <- list(
-#    EXPERIMENT = list(
-#        PATHS = list(
-#            DROPBOX = "/mnt/c/Users/%s/Dropbox (MIT)/",
-#            CONFIG = "sampleGridConfig.R"
-#        ),
-#
-#        DIRECTORIES = c(
-#            "peak",
-#            "fastq",
-#            "alignment",
-#            "qualityControl",
-#            "bigwig",
-#            "plots",
-#            "documentation"
-#        ),
-#
-#        OUTPUT = list(
-#            ENABLED = FALSE,
-#            CONFIG_TEMPLATE = "%s_%s.R"
-#        )
-#    ),
-#
-#    ENVIRONMENT = list(
-#        REQUIRED_VARS = c(
-#            "WINDOWS_USER"
-#        )
-#    )
-#)
-#CONFIG <- list(
-#    ID_REGEX = "\\d{5,6}",
-#    FILE_PATTERNS = list(
-#        FASTQ = "*.fastq",
-#        SAMPLE_TABLE = "sample_table"
-#    ),
-#    PATHS = list(
-#        BASE_DATA = file.path(Sys.getenv("HOME"), "data"),
-#        DOCUMENTATION = "documentation",
-#        FASTQ = "fastq"
-#    )
-#)
