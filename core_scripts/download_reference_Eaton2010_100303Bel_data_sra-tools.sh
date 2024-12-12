e01f5772059cca0da7cccd68b2eaedf35a932731
Luis <luised94@mit.edu>
2024-12-12 13:58:58 -0500

    docs(data): Add remote sync instructions
    - Add rsync command template for manual data transfer
    - Include specific path recommendations for Luria cluster
    - Document local-to-remote transfer process
    
    Impact: Improves user workflow documentation
    Related: Data transfer documentation

--- core_scripts/download_reference_Eaton2010_100303Bel_data_sra-tools.sh
+++ core_scripts/download_reference_Eaton2010_100303Bel_data_sra-tools.sh
@@ -178,3 +178,7 @@ for gsm in "${!gsm_groups[@]}"; do
 done
 
 echo "Consolidation completed"
+
+echo -e "\nTo sync data manually, run the following command:\n"
+echo "rsync -avzP LOCAL_DIR/ REMOTE_HOST:REMOTE_DIR/"
+echo -e "\nReplace:\nLOCAL_DIR with ${HOME}/data/100303Bel\nREMOTE_HOST with your Luria username@luria.mit.edu\nREMOTE_DIR with ~/data/100303Bel"
