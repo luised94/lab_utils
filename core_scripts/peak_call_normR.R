8124669c7fbc5c620ff1657c63d47a107c7be94e
Luis <luised94@mit.edu>
2024-12-13 14:55:27 -0500

    - Change experiment_id.

--- core_scripts/peak_call_normR.R
+++ core_scripts/peak_call_normR.R
@@ -91,7 +91,7 @@ for (pkg in required_packages) {
 ################################################################################
 # Directory Setup
 ################################################################################
-experiment_id <- "241010Bel"  # !! UPDATE THIS
+experiment_id <- "100303Bel"  # !! UPDATE THIS
 base_dir <- file.path(Sys.getenv("HOME"), "data", experiment_id)
 bam_dir <- file.path(base_dir, "alignment")
 peak_dir <- file.path(base_dir, "peak")
