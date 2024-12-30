87e18098263ee865c9732ba6211330d91f132c6f
luised94 <luised94@mit.edu>
2024-12-30 16:36:45 -0500

    - Fix case-sensitive error.

--- core_scripts/generate_genomic_tracks_of_eaton.R
+++ core_scripts/generate_genomic_tracks_of_eaton.R
@@ -21,7 +21,7 @@ source(file.path(Sys.getenv("HOME"), "lab_utils", "core_scripts", "functions_for
 # Parse arguments and validate configurations
 args <- parse_args(commandArgs(trailingOnly = TRUE))
 experiment_id <- args[["experiment-id"]]
-source(file.path("~/data", experiment_id, "Documentation", 
+source(file.path("~/data", experiment_id, "documentation", 
                 paste0(experiment_id, "_bmc_config.R")))
 validate_configs(c("RUNTIME_CONFIG", "EXPERIMENT_CONFIG"))
 # Load Required Libraries
