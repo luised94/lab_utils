3092521adf93ed6f7890d54b26e68d8e974328de
Luis <luised94@mit.edu>
2025-02-21 14:16:56 -0500

    refactor(quality_control/frip_fingerprint): Move commented code for extracting fasta and analyzing overlap to own file. - Isolate more quality control type data. - Extraction of fasta sequences and overlap analysis will be performed   in R.

--- reference_code/test_quality_control_after_peak_calling_using_frip_and_fingerprint.sh
+++ reference_code/test_quality_control_after_peak_calling_using_frip_and_fingerprint.sh
@@ -27,20 +27,3 @@
 #    --labels Reference \
 #    -o "$OUTDIR/fingerprint_reference.png" \
 #    --numberOfProcessors "$THREADS"
-#
-## === Peak Comparison ===
-#bedtools intersect \
-#    -a "$OUTDIR/test_peaks/${OUTPUT_PREFIX}_test_peaks.narrowPeak" \
-#    -b "$OUTDIR/reference_peaks/${OUTPUT_PREFIX}_reference_peaks.narrowPeak" \
-#    -wa -wb > "$OUTDIR/peak_overlap.tsv"
-#
-## === Sequence Extraction ===
-#for sample_type in 'test' 'reference'; do
-#    bedtools getfasta \
-#        -fi "$GENOME_FASTA" \
-#        -bed "$OUTDIR/${sample_type}_peaks/${OUTPUT_PREFIX}_${sample_type}_peaks.narrowPeak" \
-#        -fo "$OUTDIR/${sample_type}_peaks/peak_sequences.fa" \
-#        -name
-#done
-#
-#echo "Pipeline completed successfully. Results in: $OUTDIR"
