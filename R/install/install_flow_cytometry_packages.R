21d9ffe09e42e289fbec333ac30d29a8493a88f5
luised94 <luised94@mit.edu>
2025-03-10 21:35:14 -0400

    feat(flow_cytometry/installation): Fixed boost conflicts with cytolib - Install version 1.75 of boost headers. - Install flow cytometry libraries from github.

--- R/install/install_flow_cytometry_packages.R
+++ R/install/install_flow_cytometry_packages.R
@@ -4,27 +4,29 @@ message("Starting installation script for flow cytometry packages...")
 
 # Define repository root
 repository_root <- "~/lab_utils"
-
-# Define required packages
-packages_to_install <- c("flowCore")
-repository <- "github::"
-user <- "RGLab/"
-packages <- c("RProtoBufLib", "cytolib", "flowCore")
-packages_to_install <- paste0(repository, user, packages)
-renv::install(c(packages_to_install, "ggcyto"))
-
 if (!dir.exists(repository_root)) {
   stop("Error: Repository root not found at ", repository_root)
 }
 
+# Define required packages
+# In R console with activated renv
+# Because of issues with boost libraries (updated methods are not the same as methods used by flowCore), you must install a particular version of boost library.
+#remove.packages(c("BH", "flowCore", "cytolib"))
+
+renv::install("BH", version = "1.75.0-0")
+repository <- "github::"
+user <- "RGLab/"
+packages <- c("RProtoBufLib", "cytolib", "flowCore")
+packages_to_install <- paste0(repository, user, packages)
+
 # Activate renv
 message("Activating renv environment...")
 
-renv_path <- file.path(repository_root, "renv/activate.R")
+renv_activation_path <- file.path(repository_root, "renv/activate.R")
 if (!file.exists(renv_path)) {
     stop("Error: renv environment not found at ", renv_path)
 }
-source(renv_path)
+source(renv_activation_path)
 
 # Install and verify packages
 message("Installing and verifying genomics packages...")
@@ -42,3 +44,4 @@ for (package in packages_to_install) {
 
 # Display success message
 message("\nAll packages installed and verified successfully!")
+#renv::snapshot()
