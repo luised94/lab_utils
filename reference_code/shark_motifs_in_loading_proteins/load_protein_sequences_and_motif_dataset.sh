a652f3bd9ac0d11448dd3f94fa262c6974599aa7
luised94 <luised94@mit.edu>
2025-04-04 12:05:54 -0400

    feat(shark_analysis/load,docs): Initial commit of branch - Create data loading script - Create documentation - Create analysis script

---
+++ reference_code/shark_motifs_in_loading_proteins/load_protein_sequences_and_motif_dataset.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Description: Download datasets required to see SHARK motifs in helicase loading proteins
+# Use curl, wget and rest api to obtain datasets
+# Usage: Run on the command line or as script.
+
+####################
+# Configuration
+####################
+
