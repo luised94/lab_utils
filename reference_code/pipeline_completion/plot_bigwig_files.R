c308636c9b276fd0e1a2cf7add9929019fb8bb70
Luis <luised94@mit.edu>
2025-04-07 12:35:39 -0400

    feat(pipeline_completion/plot): uncomment genome/feature load, add main for loop processing for loading bigwig and plotting - Uncomment genome and feature loading section - Use key method to process samples in for loop

--- reference_code/pipeline_completion/plot_bigwig_files.R
+++ reference_code/pipeline_completion/plot_bigwig_files.R
@@ -43,53 +43,53 @@ REF_GENOME_FILE <- list.files(
     recursive = TRUE
 )[1]
 
-#if (length(REF_GENOME_FILE) == 0) {
-#    stop(sprintf("No reference genome files found matching pattern '%s' in: %s",
-#                FILE_GENOME_DIRECTORY,
-#                FILE_FEATURE_DIRECTORY))
-#}
-#
-#if (!file.exists(REF_GENOME_FILE)) {
-#    stop(sprintf("Reference genome file not accessible: %s", REF_GENOME_FILE[1]))
-#}
+if (length(REF_GENOME_FILE) == 0) {
+    stop(sprintf("No reference genome files found matching pattern '%s' in: %s",
+                FILE_GENOME_DIRECTORY,
+                FILE_FEATURE_DIRECTORY))
+}
 
-#REFERENCE_GENOME_DSS <- Biostrings::readDNAStringSet(REF_GENOME_FILE)
-#
-## Create chromosome range
-#CHROMOSOME_TO_PLOT <- RUNTIME_CONFIG$process_chromosome
-#CHROMOSOME_WIDTH <- REFERENCE_GENOME_DSS[CHROMOSOME_TO_PLOT]@ranges@width
-#CHROMOSOME_ROMAN <- paste0("chr", utils::as.roman(CHROMOSOME_TO_PLOT))
-#
-#GENOME_RANGE_TO_LOAD <- GenomicRanges::GRanges(
-#    seqnames = CHROMOSOME_ROMAN,
-#    ranges = IRanges::IRanges(start = 1, end = CHROMOSOME_WIDTH),
-#    strand = "*"
-#)
-#
-## Load feature file (annotation)
-#FEATURE_FILE <- list.files(
-#    path = GENOME_TRACK_CONFIG$file_feature_directory,
-#    pattern = GENOME_TRACK_CONFIG$file_feature_pattern,
-#    full.names = TRUE
-#)[1]
-#
-#if (length(FEATURE_FILE) == 0)
-#{
-#    warning(sprintf("No feature files found matching pattern '%s' in: %s",
-#                   GENOME_TRACK_CONFIG$file_feature_pattern,
-#                   GENOME_TRACK_CONFIG$file_feature_directory))
-#}
-#
-#if (!is.null(FEATURE_FILE))
-#{
-#    GENOME_FEATURES <- rtracklayer::import(FEATURE_FILE)
-#    # Convert to chrRoman format
-#    GenomeInfoDb::seqlevels(GENOME_FEATURES) <- paste0(
-#        "chr",
-#        utils::as.roman(gsub("chr", "", GenomeInfoDb::seqlevels(GENOME_FEATURES)))
-#    )
-#}
-#
+if (!file.exists(REF_GENOME_FILE)) {
+    stop(sprintf("Reference genome file not accessible: %s", REF_GENOME_FILE[1]))
+}
+
+REFERENCE_GENOME_DSS <- Biostrings::readDNAStringSet(REF_GENOME_FILE)
+
+# Create chromosome range
+CHROMOSOME_TO_PLOT <- RUNTIME_CONFIG$process_chromosome
+CHROMOSOME_WIDTH <- REFERENCE_GENOME_DSS[CHROMOSOME_TO_PLOT]@ranges@width
+CHROMOSOME_ROMAN <- paste0("chr", utils::as.roman(CHROMOSOME_TO_PLOT))
+
+GENOME_RANGE_TO_LOAD <- GenomicRanges::GRanges(
+    seqnames = CHROMOSOME_ROMAN,
+    ranges = IRanges::IRanges(start = 1, end = CHROMOSOME_WIDTH),
+    strand = "*"
+)
+
+# Load feature file (annotation)
+FEATURE_FILE <- list.files(
+    path = GENOME_TRACK_CONFIG$file_feature_directory,
+    pattern = GENOME_TRACK_CONFIG$file_feature_pattern,
+    full.names = TRUE
+)[1]
+
+if (length(FEATURE_FILE) == 0)
+{
+    warning(sprintf("No feature files found matching pattern '%s' in: %s",
+                   GENOME_TRACK_CONFIG$file_feature_pattern,
+                   GENOME_TRACK_CONFIG$file_feature_directory))
+}
+
+if (!is.null(FEATURE_FILE))
+{
+    GENOME_FEATURES <- rtracklayer::import(FEATURE_FILE)
+    # Convert to chrRoman format
+    GenomeInfoDb::seqlevels(GENOME_FEATURES) <- paste0(
+        "chr",
+        utils::as.roman(gsub("chr", "", GenomeInfoDb::seqlevels(GENOME_FEATURES)))
+    )
+}
+
 ################################################################################
 # Setup directories and file paths
 ################################################################################
@@ -154,13 +154,14 @@ filenames_without_extension <- gsub(FILE_EXTENSION_TO_REMOVE, "", file_basenames
 split_metadata <- strsplit(filenames_without_extension, split = "_")
 
 underscore_counts <- lengths(gregexpr("_", filenames_without_extension))
+NUMBER_OF_COLUMNS <- length(COLUMN_NAMES)
 
 if (length(unique(underscore_counts)) != 1 || unique(underscore_counts) != EXPECTED_NUM_UNDERSCORES) {
   stop("Filenames must contain exactly ", EXPECTED_NUM_UNDERSCORES, " underscores.")
 }
 
 # Preallocation
-bigwig_metadata_df <- data.frame(matrix(NA, nrow = NUMBER_OF_FILES, ncol = underscore_counts + 1 + 1 ))
+bigwig_metadata_df <- data.frame(matrix(NA, nrow = NUMBER_OF_FILES, ncol = NUMBER_OF_COLUMNS))
 
 for (row_index in 1:nrow(bigwig_metadata_df)) {
     bigwig_metadata_df[row_index, 1:3] <- split_metadata[[row_index]]
@@ -174,16 +175,93 @@ stopifnot(
     "No samples are NA." = !any(is.na(bigwig_metadata_df$sample)),
     "Metadata has expected dimensions." =
         nrow(bigwig_metadata_df) == NUMBER_OF_FILES &&
-        ncol(bigwig_metadata_df) == length(COLUMN_NAMES) + 1,
+        ncol(bigwig_metadata_df) == length(COLUMN_NAMES),
     "No duplicate samples." = !any(duplicated(bigwig_metadata_df$sample))
 )
 
 if (length(BIGWIG_FILES) == 0) {
     stop("No bigwig files found.")
 }
+
 #################################################################################
-## Setup configuration for genome track plots
+## MAIN
 #################################################################################
+# Define categories
+CATEGORIES <- list(
+    samples = unique(bigwig_metadata_df[,"sample"]),
+    bam_processing = unique(bigwig_metadata_df[,"bam_processing"]),
+    bigwig_processing = unique(bigwig_metadata_df[, "bigwig_processing"])
+)
+
+sample_bam_combinations <- expand.grid(
+    CATEGORIES[c("samples", "bam_processing")]
+)
+
+
+# Handle the sample and bam_processing combinations
+COLUMN_SEPARATOR <-  "\x01"
+metadata_chr <- lapply(bigwig_metadata_df[c("sample", "bam_processing")], as.character)
+metadata_keys <- do.call(paste, c(metadata_chr, sep = COLUMN_SEPARATOR))
+
+control_combos_chr <- lapply(sample_bam_combinations, as.character)
+control_keys <- do.call(paste, c(control_combos_chr, sep = COLUMN_SEPARATOR))
+
+for (key_idx in 1:length(control_keys)) {
+    control_key <- control_keys[key_idx]
+    keys_in_current_key <- metadata_keys %in% control_key
+    cat(sprintf("Processing %s key\n", key_idx))
+    cat(sprintf("Using key = %s \n", control_key))
+
+
+    rows_to_analyze <- bigwig_metadata_df[keys_in_current_key, ]
+    message("Sample subsetting complete...")
+    print(head(rows_to_analyze))
+
+    track_list <- vector("list", nrow(rows_to_analyze) + 1 + 1)
+    track_list[[1]] <- Gviz::GenomeAxisTrack(
+        name = sprintf("Chr %s Axis", CHROMOSOME_TO_PLOT)
+    )
+
+    for (i in seq_along(track_list)) {
+         bigwig_data <- rtracklayer::import(
+            rows_to_analyze[i, "file_paths"],
+            format = "BigWig",
+            which = GENOME_RANGE_TO_LOAD
+        )
+        track_list[[i + 1]] <- Gviz::DataTrack(bigwig_data)
+        message(sprint("Sample %s imported...", i))
+
+    }
+
+    if (exists("features")) {
+        tracks[[length(tracks)]] <- Gviz::AnnotationTrack(
+            GENOME_FEATURES,
+            name = "Features",
+            size = 0.5,
+            background.title = "lightgray",
+            fontcolor.title = "black",
+            cex.title = 0.6
+        )
+    }
+
+    Gviz::plotTracks(
+        trackList = track_list
+    )
+}
+
+# 
+#sample_bigwig_combinations <- expand.grid(
+#    c(CATEGORIES["samples"], 
+#        bigwig_processing = "cpm")
+#)
+#metadata_chr <- lapply(bigwig_metadata_df[c("sample", "bigwig_processing")], as.character)
+#metadata_keys <- do.call(paste, c(metadata_chr, sep = COLUMN_SEPARATOR))
+#
+#control_combos_chr <- lapply(sample_bigwig_combinations, as.character)
+#control_keys <- do.call(paste, c(control_combos_chr, sep = COLUMN_SEPARATOR))
+#is_not_input <- !grepl("input", control_keys)
+#control_keys <- control_keys[is_not_input]
+# Determine preallocation parameters for tracks list
 ## Initialize track list with genome axis
 #tracks <- list(
 #    Gviz::GenomeAxisTrack(
