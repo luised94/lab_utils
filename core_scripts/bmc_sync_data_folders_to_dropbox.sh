fe963f9b6c7684b078d117bec0cf5cd35b6b173a
Luis <luised94@mit.edu>
2025-10-02 15:35:39 -0400

    feat(bmc/sync): New script to sync ngs data to dropbox folders

---
+++ core_scripts/bmc_sync_data_folders_to_dropbox.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [[ -z "${DROPBOX_PATH}" ]]; then
+    echo "Error: DROPBOX_PATH not set. Set manually before running."
+    echo "DROPBOX_PATH is set via my_config repository."
+    echo "Current value: || $DROPBOX_PATH ||"
+    exit 1
+fi
+
+FROM_DIR="$HOME/data/"
+TO_DIR="${DROPBOX_PATH}/Lab/Experiments/ngs/"
+
+if [[ ! -d "${FROM_DIR}" ]]; then
+    echo "Error: Experiment directory does not exist: ${EXPERIMENT_DIR}"
+    exit 1
+fi
+
+if [[ ! -d "${TO_DIR}" ]]; then
+    echo "Error: Experiment directory does not exist: ${EXPERIMENT_DIR}"
+    exit 1
+fi
+
+mapfile -t ALL_DIRECTORIES < <(find "${FROM_DIR}" -mindepth 1 -maxdepth 1 -type d)
+
+for directories in "${ALL_DIRECTORIES[@]}"; do
+  echo "----------------------------------------"
+
+  base_directory_name=$(basename "${directories}" )
+  echo "Current directory|| ${directories} ||"
+  echo "Basename|| ${base_directory_name} ||"
+
+  echo "----------------------------------------"
+done
