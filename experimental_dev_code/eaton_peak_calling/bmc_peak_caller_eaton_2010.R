217e1d4d273a6321e9b27f077bd601b6d785a8fb
luised94 <luised94@mit.edu>
2025-10-11 16:10:07 -0400

    feat(bmc/peak_calling): Reorganize code into input and config then logic - Try to add suffix for a simple type - Try to add namespace to functions too - Consolidate calculations into variables - Untested

--- experimental_dev_code/eaton_peak_calling/bmc_peak_caller_eaton_2010.R
+++ experimental_dev_code/eaton_peak_calling/bmc_peak_caller_eaton_2010.R
@@ -1,99 +1,155 @@
+# Attempt to replicate eaton peak calling protocol found in supplementary.
+# Eaton et al 2010. CitationKey: Eaton2010conserved
 library(GenomicAlignments)
 library(rtracklayer)
 library(Rsamtools)
 
-REFERENCE_BAM <- "/home/luised94/data/100303Bel/alignment/consolidated_034475_sequence_to_S288C_sorted.bam"
-reference_bam_header <- Rsamtools::scanBamHeader(REFERENCE_BAM)
-sample_chromosomes <- names(reference_bam_header[[REFERENCE_BAM]]$targets)
-chromosome_name <- sample_chromosomes[1]
-chromosome_length <- chromosome_lengths[1]
-which_chr <- GRanges(
-  chromosome_name, 
-  IRanges(1, reference_bam_header[[REFERENCE_BAM]]$targets[[chromosome_name]])
+# INPUTS
+# REQUIRED
+# CHIP reads as bam (turn into grange or galignments)
+REFERENCE_BAM_PATH <- "/home/luised94/data/100303Bel/alignment/consolidated_034475_sequence_to_S288C_sorted.bam"
+# ORF annotations - filter the gff after loading
+# @FIX: Score column has CDS. Need to fix during the import
+#orf_bed_file_path <- "~/data/feature_files/20250423_orf_sgd.bed"
+GFF_FILE_PATH <- "~/data/feature_files/240830_saccharomyces_cerevisiae.gff"
+
+# OPTIONAL
+# INPUT_BAM <- ""
+# BLACKLIST_BED_FILE_PATH <- ""
+
+# Configuration
+THRESHOLD_FOLD <- 3
+CROSS_CORR_MIN_OFFSET_bp <- 100
+CROSS_CORR_MAX_OFFSET_bp <- 300
+CROSS_CORR_STEP_bp <- 10
+CROSS_CORR_BIN_SIZE_bp <- 20
+SLIDING_WINDOWS_STEP_SIZE_bp <- 25
+PVALUE_THRESHOLD <- 10^-6
+WINDOW_MAX_MERGE_GAP_bp <- 50
+MIN_REPLICATE_OVERLAP_pct <- 0.5
+CANDIDATE_WINDOW_SIZE_bp <- 300
+ENRICHMENT_FOLD <- 3
+CANDIDATE_REGION_MIN_SIZE_bp <- 500
+OFFSETS_bp <- seq(
+  from = CROSS_CORR_MIN_OFFSET_bp,
+  to = CROSS_CORR_MAX_OFFSET_bp,
+  by = CROSS_CORR_STEP_bp
 )
 
-reads_chrI <- readGAlignments(
-    REFERENCE_BAM, 
-    param = ScanBamParam(which = which_chr)
+
+# Import the GFF file. rtracklayer will parse all the rich metadata.
+gff_annotations <- import(GFF_FILE_PATH)
+REFERENCE_BAM_HEADER <- Rsamtools::scanBamHeader(REFERENCE_BAM_PATH)
+CHROMOSOME_LENGTHS_nls <- REFERENCE_BAM_HEADER[[REFERENCE_BAM_PATH]]$targets
+CHROMOSOME_NAMES_chr <- names(CHROMOSOME_LENGTHS_nls)
+CHROMOSOME_NAME_chr <- CHROMOSOME_NAMES_chr[1]
+chromosome_length <- CHROMOSOME_LENGTHS_nls[1]
+WHICH_CHR_gr <- GenomicRanges::GRanges(
+  CHROMOSOME_NAME_chr,
+  IRanges(
+    1,
+    CHROMOSOME_LENGTHS_nls[[CHROMOSOME_NAME_chr]]
+  )
 )
-reads_by_strand <- split(reads_chrI, strand(reads_chrI))
-coverage_plus <- coverage(reads_by_strand$`+`, width = unname(chromosome_length))
-coverage_minus <- coverage(reads_by_strand$`-`, width = unname(chromosome_length))
-windows_300bp <- tileGenome(
-  chromosome_length, 
-  tilewidth = 300,
+
+READS_CHRI_galn <- GenomicAlignments::readGAlignments(
+  REFERENCE_BAM_PATH,
+  param = ScanBamParam(which = WHICH_CHR_gr)
+)
+
+WINDOW_TILES_vct <- GenomicRanges::tileGenome(
+  seqlengths = chromosome_length,
+  tilewidth = CANDIDATE_WINDOW_SIZE_bp,
   cut.last.tile.in.chrom = TRUE
 )
-se <- summarizeOverlaps(
-  features = windows_300bp, 
-  reads = reads_chrI,
+
+reads_by_strand <- split(
+  READS_CHRI_galn,
+  strand(READS_CHRI_galn)
+)
+plus_strand_reads <- reads_by_strand$`+`
+minus_strand_reads <- reads_by_strand$`-`
+plus_gr <- granges(plus_strand_reads)
+minus_gr <- granges(minus_strand_reads)
+coverage_plus <- coverage(plus_strand_reads, width = unname(chromosome_length))
+coverage_minus <- coverage(minus_strand_reads, width = unname(chromosome_length))
+
+se <- GenomicAlignments::summarizeOverlaps(
+  features = WINDOW_TILES_vct,
+  reads = READS_CHRI_galn,
   ignore.strand = TRUE
 )
-mean_score <- mean(assay(se)[,1])
-threshold_fold <- 3
-threshold_score <- mean_score * threshold_fold
-is_candidate_window <- assay(se)[,1] >= threshold_score
-scores_above_threshold <- assay(se)[,1][is_candidate_window]
-candidate_windows <- windows_300bp[is_candidate_window]
+score_by_window <- assay(se)[,1]
+mean_score <- mean(score_by_window)
+threshold_score <- mean_score * THRESHOLD_FOLD
+is_candidate_window <- score_by_window >= threshold_score
+scores_above_threshold <- score_by_window[is_candidate_window]
+candidate_windows <- WINDOW_TILES_vct[is_candidate_window]
 candidate_regions <- reduce(candidate_windows, ignore.strand = TRUE)
 
-overlaps <- findOverlaps(candidate_regions, candidate_windows)
+overlaps <- GenomicAlignments::findOverlaps(
+  query = candidate_regions,
+  subject = candidate_windows
+)
 num_windows <- countQueryHits(overlaps)
 mcols(candidate_regions)$num_merged_windows <- num_windows
-large_candidate_regions <- candidate_regions[width(candidate_regions) >= 500]
-
-# Define offsets in base pairs
-offsets <- seq(100, 300, by = 10)
+large_candidate_regions <- candidate_regions[width(candidate_regions) >= CANDIDATE_REGION_MIN_SIZE_bp]
 
 # Create a list to store the results
 cross_corr_results <- lapply(seq_along(large_candidate_regions), function(i) {
-    region <- large_candidate_regions[i]
-    chrom <- as.character(seqnames(region))
+  region <- large_candidate_regions[i]
+  chrom <- as.character(seqnames(region))
 
-    # --- Bin coverage into 20bp bins ---
-    bins <- unlist(tile(region, width = 20))
-    ir_bins <- ranges(bins)
-    
-    # Get the coverage views for the bins on the appropriate chromosome
-    plus_views <- Views(coverage_plus[[chrom]], ir_bins)
-    minus_views <- Views(coverage_minus[[chrom]], ir_bins)
-    
-    # Calculate mean coverage in each bin
-    plus_binned_cov <- viewMeans(plus_views)
-    minus_binned_cov <- viewMeans(minus_views)
+  # --- Bin coverage into 20bp bins ---
+  bins <- unlist(tile(
+    region,
+    width = CROSS_CORR_BIN_SIZE_bp
+  ))
+  ir_bins <- ranges(bins)
+  
+  # Get the coverage views for the bins on the appropriate chromosome
+  plus_views <- Views(coverage_plus[[chrom]], ir_bins)
+  minus_views <- Views(coverage_minus[[chrom]], ir_bins)
+  
+  # Calculate mean coverage in each bin
+  plus_binned_cov <- viewMeans(plus_views)
+  minus_binned_cov <- viewMeans(minus_views)
 
-    # --- Shifting and Correlation ---
-    correlations <- sapply(offsets, function(offset) {
-        # Convert bp offset to bin offset. Use floor to ensure integer.
-        bin_offset <- floor(offset / 20)
-        
-        # Shift the minus strand coverage vector upstream (left shift)
-        # We need to pad the end with NAs and remove elements from the start
-        n_bins <- length(minus_binned_cov)
-        if (bin_offset >= n_bins) return(NA) # Offset is larger than the vector
-        
-        shifted_minus <- c(minus_binned_cov[(bin_offset + 1):n_bins], rep(NA, bin_offset))
-        
-        # Calculate Pearson correlation, removing NAs
-        cor_test <- cor.test(plus_binned_cov, shifted_minus, method = "pearson")
-        return(cor_test$estimate)
-    })
-
-    # Find the offset with the maximum correlation
-    if (all(is.na(correlations))) {
-        max_corr <- NA
-        best_offset <- NA
-    } else {
-        max_idx <- which.max(correlations)
-        max_corr <- correlations[max_idx]
-        best_offset <- offsets[max_idx]
-    }
+  # --- Shifting and Correlation ---
+  correlations <- sapply(OFFSETS_bp, function(offset) {
+    # Convert bp offset to bin offset. Use floor to ensure integer.
+    bin_offset <- floor(offset / 20)
     
-    return(data.frame(
-        region_id = i, 
-        max_correlation = max_corr, 
-        best_offset = best_offset
-    ))
+    # Shift the minus strand coverage vector upstream (left shift)
+    # We need to pad the end with NAs and remove elements from the start
+    n_bins <- length(minus_binned_cov)
+    if (bin_offset >= n_bins) return(NA) # Offset is larger than the vector
+    
+    shifted_minus <- c(
+      minus_binned_cov[(bin_offset + 1):n_bins],
+      rep(NA, bin_offset)
+    )
+    
+    # Calculate Pearson correlation, removing NAs
+    cor_test <- cor.test(plus_binned_cov, shifted_minus, method = "pearson")
+    return(cor_test$estimate)
+  })
+
+  # Find the offset with the maximum correlation
+  if (all(is.na(correlations))) {
+    max_corr <- NA
+    best_offset <- NA
+  } else {
+    max_idx <- which.max(correlations)
+    max_corr <- correlations[max_idx]
+    best_offset <- OFFSETS_bp[max_idx]
+  }
+  
+  return(data.frame(
+    region_id = i, 
+    max_correlation = max_corr, 
+    best_offset = best_offset
+  ))
 })
 
 # Combine results into a single data frame
@@ -110,11 +166,6 @@ window_size <- fragment_size * 2
 shift_amount <- floor(fragment_size / 2)
 
 # 2. Split reads by strand
-reads_by_strand <- split(reads_chrI, strand(reads_chrI))
-plus_strand_reads <- reads_by_strand$`+`
-minus_strand_reads <- reads_by_strand$`-`
-plus_gr <- granges(plus_strand_reads)
-minus_gr <- granges(minus_strand_reads)
 
 # 4. Resize and shift, creating potentially out-of-bounds ranges
 shifted_plus_raw <- shift(resize(plus_gr, width = 1, fix = "start"), shift = shift_amount)
@@ -128,45 +179,43 @@ cat("Number of out-of-bounds plus-strand fragments:", out_of_bounds_plus, "\n")
 cat("Number of out-of-bounds minus-strand fragments:", out_of_bounds_minus, "\n")
 fragment_centers <- c(trimmed_plus, trimmed_minus)
 strand(fragment_centers) <- "*"
-# Import the BED file into a GRanges object
-# Score column has CDS. Need to fix during the import
-#orf_bed_file_path <- "~/data/feature_files/20250423_orf_sgd.bed"
-#orf_annotations <- import(orf_bed_file_path)
-gff_file_path <- "~/data/feature_files/240830_saccharomyces_cerevisiae.gff"
-
-# Import the GFF file. rtracklayer will parse all the rich metadata.
-gff_annotations <- import(gff_file_path)
 
 # --- Inspection ---
-print("Summary of the imported GFF annotations:")
-print(gff_annotations)
+#print("Summary of the imported GFF annotations:")
+#print(gff_annotations)
 
 # Inspect the feature types available in the file
-print("Available feature types in the GFF file:")
-print(table(mcols(gff_annotations)$type))
+#print("Available feature types in the GFF file:")
+#print(table(mcols(gff_annotations)$type))
 
 # --- 1. Generate Sliding Windows ---
 # Define the step size for the sliding windows
-step_size <- 25
-
 # Create a GRanges object representing all sliding windows across the genome
 # This works by creating all start sites and then defining ranges of 'window_size'
 window_starts <- lapply(
-  names(chromosome_lengths), function(chrom) {
-  seq(
-    from = 1,
-    to = chromosome_lengths[chrom] - window_size + 1,
-    by = step_size
-  )
-})
+  names(CHROMOSOME_LENGTHS_nls),
+  function(chrom) {
+    seq(
+      from = 1,
+      to = CHROMOSOME_LENGTHS_nls[chrom] - window_size + 1,
+      by = step_size
+    )
+  }
+)
 
 sliding_windows <- GRanges(
   seqnames = rep(
-        names(chromosome_lengths),
-        lengths(window_starts)
+    names(CHROMOSOME_LENGTHS_nls),
+    lengths(window_starts)
   ),
-  ranges = IRanges(start = unlist(window_starts), width = window_size),
-  seqinfo = seqinfo(orf_annotations) # Use seqinfo from annotations for consistency
+  ranges = IRanges(
+    start = unlist(window_starts), 
+    width = window_size
+  ),
+  # Use seqinfo from annotations for consistency
+  seqinfo = seqinfo(
+    gff_annotations
+  )
 )
 
 ######### JUNK ###########
@@ -177,13 +226,13 @@ sliding_windows <- GRanges(
 #FILE_GENOME_PATTERN <- "S288C_refgenome.fna"
 #FILE_FEATURE_PATTERN <- "eaton_peaks"
 ## @ques: maybe I should download this file locally.
-#REFERENCE_BAM <- "/home/luised94/data/100303Bel/alignment/consolidated_034475_sequence_to_S288C_sorted.bam"
-#reference_bam_header <- Rsamtools::scanBamHeader(REFERENCE_BAM)
-#print(reference_bam_header[[REFERENCE_BAM]]$targets)
+#REFERENCE_BAM_PATH <- "/home/luised94/data/100303Bel/alignment/consolidated_034475_sequence_to_S288C_sorted.bam"
+#REFERENCE_BAM_HEADER <- Rsamtools::scanBamHeader(REFERENCE_BAM_PATH)
+#print(CHROMOSOME_LENGTHS_nls)
 #
 #stopifnot(
-#  #"REFERENCE_BAM does not exist." =
-#  #file.exists(REFERENCE_BAM),
+#  #"REFERENCE_BAM_PATH does not exist." =
+#  #file.exists(REFERENCE_BAM_PATH),
 #  "Genome directory not found" =
 #  dir.exists(FILE_GENOME_DIRECTORY),
 #  "Feature directory not found" =
@@ -214,8 +263,8 @@ sliding_windows <- GRanges(
 #missing_packages <- required_packages[is_missing_package]
 #if (length(missing_packages) > 0 ) {
 #  stop(
-#    "Missing packages. Please install using renv:\n",
-#    paste(missing_packages, collapse = ", ")
+#  "Missing packages. Please install using renv:\n",
+#  paste(missing_packages, collapse = ", ")
 #  )
 #}
 #
@@ -237,15 +286,15 @@ sliding_windows <- GRanges(
 #
 #if (length(FEATURE_FILE) == 0) {
 #  warning(sprintf("No feature files found matching pattern '%s' in: %s",
-#    FILE_FEATURE_PATTERN,
-#    FILE_FEATURE_DIRECTORY
+#  FILE_FEATURE_PATTERN,
+#  FILE_FEATURE_DIRECTORY
 #  ))
 #}
 #
 #if (length(REF_GENOME_FILE) == 0) {
 #  stop(sprintf("No reference genome files found matching pattern '%s' in: %s",
-#    FILE_GENOME_DIRECTORY,
-#    FILE_FEATURE_DIRECTORY
+#  FILE_GENOME_DIRECTORY,
+#  FILE_FEATURE_DIRECTORY
 #  ))
 #}
 #
@@ -263,9 +312,8 @@ sliding_windows <- GRanges(
 ##  message("Current chromosome: ", chromosome)
 ##
 ##}
-#message("REFERENCE chromosome is same as sample chromosomes: ", as.character(identical(ALL_CHROMOSOMES, names(reference_bam_header[[REFERENCE_BAM]]$targets))))
-#chromosome_lengths <- reference_bam_header[[REFERENCE_BAM]]$targets
+#message("REFERENCE chromosome is same as sample chromosomes: ", as.character(identical(ALL_CHROMOSOMES, names(CHROMOSOME_LENGTHS_nls))))
 #
-##reads_plus <- reads_chrI[strand(reads_chrI) == "+"]
+##reads_plus <- READS_CHRI_galn[strand(READS_CHRI_galn) == "+"]
 ##coverage_plus <- coverage(reads_plus, width = unname(chromosome_length))
-##summarizeOverlaps(features = windows_300bp, reads = REFERENCE_BAM, ignore.strand = TRUE)
+##summarizeOverlaps(features = WINDOW_TILES_vct, reads = REFERENCE_BAM_PATH, ignore.strand = TRUE)
