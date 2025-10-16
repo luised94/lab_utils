b6b3841777165e9a6e145109619c90050ed178c9
luised94 <luised94@mit.edu>
2025-10-15 21:08:17 -0400

    feat(bmc/single_vs_paired_end_reads): Extract idx variables for extraction, add read pairs number determination

--- experimental_dev_code/single_vs_paired_end_reads/determining_single_vs_paired_end_reads.sh
+++ experimental_dev_code/single_vs_paired_end_reads/determining_single_vs_paired_end_reads.sh
@@ -3,32 +3,28 @@
 DIRECTORY="$HOME/data/250930Bel/fastq"
 NUMBER_OF_LANES=3
 IS_PAIRED_END=1
+FASTQ_FILES_PER_LANE=
+# Example of filename: 250930Bel_D25-12496-2_2_sequence.fastq
+# Check the fastq files using just _ and -_?
+IDX_FOR_ID_PART=2
+IDX_FOR_LANE_PART=3
+IDX_FOR_READ_PAIR_PART=4
 
 if [[ IS_PAIRED_END -eq 1 ]];then
-  EXPECTED_NUMBER_OF_FASTQ_FILES_PER_SAMPLE=$(( NUMBER_OF_LANES * 2))
-else
-  EXPECTED_NUMBER_OF_FASTQ_FILES_PER_SAMPLE=$(( NUMBER_OF_LANES * 1))
-fi
-echo "Expected number of fastq files: $EXPECTED_NUMBER_OF_FASTQ_FILES_PER_SAMPLE"
+  FASTQ_FILES_PER_LANE=2
+  EXPECTED_NUMBER_OF_FASTQ_FILES_PER_SAMPLE=$(( NUMBER_OF_LANES * FASTQ_FILES_PER_LANE))
 
+else
+  FASTQ_FILES_PER_LANE=1
+  EXPECTED_NUMBER_OF_FASTQ_FILES_PER_SAMPLE=$(( NUMBER_OF_LANES * FASTQ_FILES_PER_LANE))
+
+fi
+
+
+echo "Expected number of fastq files: $EXPECTED_NUMBER_OF_FASTQ_FILES_PER_SAMPLE"
 mapfile -t fastq_files < <(find "$DIRECTORY" -type f -name "*.fastq")
 
-cat ${fastq_files[6]} | head -n 8
-cat ${fastq_files[7]} | head -n 8
-
-cat ${fastq_files[6]} | wc -l
-cat ${fastq_files[7]} | wc -l
-
-# Example of filename: 250930Bel_D25-12496-2_2_sequence.fastq
-for fastq_file in "${fastq_files[@]}"; do
-  if [[ "$fastq_file" =~ unmapped ]]; then
-    echo "Skipping unmapped fastq file"
-    continue
-  fi
-  echo "$fastq_file"
-
-done
-
+echo "Getting unique_ids"
 readarray -t unique_ids < <(
   for fastq_file in "${fastq_files[@]}"; do
     if [[ "$fastq_file" =~ unmapped ]]; then
@@ -38,25 +34,41 @@ readarray -t unique_ids < <(
     # Split filename components
     IFS='_-' read -ra parts <<< "${fastq_file##*/}"
     # Extract ID from standardized position (3rd component)
-    printf "%s\n" "${parts[2]}"
+    printf "%s\n" "${parts[$IDX_FOR_ID_PART]}"
+  done | sort -u | grep -v "unmapped"
+)
+
+echo "Getting unique lanes..."
+readarray -t unique_lanes < <(
+  for fastq_file in "${fastq_files[@]}"; do
+    if [[ "$fastq_file" =~ unmapped ]]; then
+      echo "Skipping unmapped fastq file"
+      continue
+    fi
+    # Split filename components
+    IFS='_-' read -ra parts <<< "${fastq_file##*/}"
+    # Extract ID from standardized position (3rd component)
+    printf "%s\n" "${parts[$IDX_FOR_LANE_PART]}"
+  done | sort -u | grep -v "unmapped"
+)
+
+echo "Getting unique read pairs..."
+readarray -t unique_read_pairs_id < <(
+  for fastq_file in "${fastq_files[@]}"; do
+    if [[ "$fastq_file" =~ unmapped ]]; then
+      echo "Skipping unmapped fastq file"
+      continue
+    fi
+    # Split filename components
+    IFS='_-' read -ra parts <<< "${fastq_file##*/}"
+    # Extract ID from standardized position (3rd component)
+    printf "%s\n" "${parts[$IDX_FOR_READ_PAIR_PART]}"
   done | sort -u | grep -v "unmapped"
 )
 
 printf '%s\n' "${unique_ids[@]}" | xargs -n6 | sed 's/^/  /' | column -t
-for id in "${unique_ids[@]}"; do
-  if [ ${#files[@]} -eq 0 ]; then
-    echo "ERROR: No files found for ID: $id"
-    continue
-  fi
-
-  if [ ! ${#files[@]} -eq 2 ]; then
-    echo -e "[ERROR]: Found ${#files[@]} for $id\nExpected 2"
-    continue
-  fi
-
-done
-#echo "${unique_ids[@]}"
-#echo "${fastq_file[@]}" | awk -F/ '{print $NF}'
+printf '%s\n' "${unique_lanes[@]}" | xargs -n6 | sed 's/^/  /' | column -t
+printf '%s\n' "${unique_read_pairs_id[@]}" | xargs -n6 | sed 's/^/  /' | column -t
 
 for id in "${unique_ids[@]}"; do
   echo "--------------------"
@@ -65,14 +77,35 @@ for id in "${unique_ids[@]}"; do
   #files=( *"${id}"*_sequence.fastq )
   mapfile -t files < <( find "$DIRECTORY" -type f -name "*${id}*" )
 
-  if [[ ${#files[@]} -eq 0 ]]; then
+  if [[ ${#files_with_id[@]} -eq 0 ]]; then
     echo "ERROR: No files found for ID: $id"
     continue
   fi
 
-  if [[ ! ${#files[@]} -eq $EXPECTED_NUMBER_OF_FASTQ_FILES_PER_SAMPLE ]]; then
-    echo -e "[ERROR]: Found ${#files[@]} for $id\nExpected 2"
+  if [[ ! ${#files_with_id[@]} -eq $EXPECTED_NUMBER_OF_FASTQ_FILES_PER_SAMPLE ]]; then
+    echo -e "[ERROR]: Found ${#files_with_id[@]} for $id\nExpected $EXPECTED_NUMBER_OF_FASTQ_FILES_PER_SAMPLE"
     continue
   fi
-  echo "Files have expected number of fastq files."
+  echo " ID '$id' has expected number of fastq files."
+
+  for lane in "${unique_lanes[@]}"; do
+    mapfile -t files_in_lane < <( find "$DIRECTORY" -type f -name "*${id}-${unique_lanes}*" )
+  done
+
+
 done
+
+#echo "${unique_ids[@]}"
+#echo "${fastq_file[@]}" | awk -F/ '{print $NF}'
+#cat ${fastq_files[6]} | head -n 8
+#cat ${fastq_files[7]} | head -n 8
+#cat ${fastq_files[6]} | wc -l
+#cat ${fastq_files[7]} | wc -l
+#for fastq_file in "${fastq_files[@]}"; do
+#  if [[ "$fastq_file" =~ unmapped ]]; then
+#    echo "Skipping unmapped fastq file"
+#    continue
+#  fi
+#  echo "$fastq_file"
+#
+#done
