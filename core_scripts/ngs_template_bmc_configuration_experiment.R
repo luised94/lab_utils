f9eb8cf3260e7b61eddac2b3d6372dc8bb937fb9
Luis <luised94@mit.edu>
2025-10-29 12:11:33 -0400

    refactor(sequencing/config): Move metadata for further subsetting to experiment config

--- core_scripts/ngs_template_bmc_configuration_experiment.R
+++ core_scripts/ngs_template_bmc_configuration_experiment.R
@@ -146,3 +146,39 @@ EXPERIMENT_CONFIG <- list(
   #),
 
 )
+# @TODO: Add once genome tracks is fixed.
+#metadata <- subset(metadata, valid_idx)
+# Set to NULL
+# Should probably move to configuration
+#columns_to_show <- c(
+#  "rescue_allele", "suppressor_allele",
+#  "cell_cycle_arrest", "repeats",
+#  "antibody", "experiment_id"
+#  )
+## Rename to focus list or something
+#message("Sourcing subset expressions for reproducible examples...")
+##EXPECTED_NUMBER_OF_REPRODUCIBLE_SAMPLES <-
+#SUBSET_REPRODUCIBLE_SAMPLES <- list(
+#    inputs = quote(
+#      antibody == "Input"
+#    ),
+#    orc_repeats_in_noco = quote(
+#      antibody == "HM1108" & cell_cycle_arrest == "NOCO" &
+#      !(repeats == "2" & experiment_id == "250324Bel")
+#    ),
+#    orc_repeats_in_alpha = quote(
+#      antibody == "HM1108" & cell_cycle_arrest == "ALPHA" &
+#      ((repeats == "1" & experiment_id == "250324Bel") |
+#      (repeats == "2" & experiment_id == "250207Bel"))
+#    ),
+#    mcm_repeats_in_alpha = quote(
+#      repeats == "2" & antibody == "UM174" & cell_cycle_arrest == "ALPHA"
+#    ),
+#    mcm_repeats_in_noco = quote(
+#      antibody == "UM174" & cell_cycle_arrest == "NOCO" &
+#      !(repeats == "1" & antibody == "UM174" & suppressor_allele %in% c("5EK", "6EK"))
+#    ),
+#    cdc6_repeats = quote(
+#      suppressor_allele == "Cdc6OE"
+#    )
+#)
