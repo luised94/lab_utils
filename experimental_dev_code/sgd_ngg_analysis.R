7e950906b5d15a8eaa74752b55d48d8057c73371
Luis <luised94@mit.edu>
2025-11-14 13:27:21 -0500

    feat(toa2/ngg_fasta): First commit - Write down issue.

---
+++ experimental_dev_code/sgd_ngg_analysis.R
@@ -0,0 +1,7 @@
+# Had to adjust plan to analyze genome with gff file.
+# Need to download those files from sgd.
+# Files were downloaded manually from SGD.
+# For now, use this script to analyze fasta files from SGD
+# After downloading to Downloads folder, run:
+# $for file in *.fsa;do mv $file "$HOME/data/fasta_files/" ; done
+# Switch mv to echo to perform dry-run.
