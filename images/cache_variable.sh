4bc62efa454bc2d130b96ab25609a5f0dcca97b8
luised94 <luised94@mit.edu>
2026-08-23 00:17:21 -0400

    chore(images): Remove superseded file.

--- images/cache_variable.sh
+++
@@ -1,2 +0,0 @@
-# source this file to set variables currently in use in the troubleshooting session
-PROFILE_CSV=/mnt/c/Users/Luised94/Desktop/lab/experiments/20260708_LM-0008_load_repeat2-KGlut300mM-allsupps/20260818_rotated_LM-0008_s0012_SDSPAGE_run-old-MCM-aliquots_Fl-Green_gel_analysis/manual_lane_profiles.csv
